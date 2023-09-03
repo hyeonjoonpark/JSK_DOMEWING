75ec9faf750251d9e1445a624d85179805bafc4f
parkchef <parkchansu39@gmail.com>
2023-09-04 01:15:31 +0800

    api.php edit

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/webhook', [WebhookController::class, 'index']);
+Route::post('/webhook', [WebhookController::class, 'webhook']);
