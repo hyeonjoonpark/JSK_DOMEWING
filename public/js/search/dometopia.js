0b641129721e3604c628a6ec9f41d3b6e9d8534e
parkchef <parkchansu39@gmail.com>
2023-09-11 17:41:09 +0800

    dometopia search fix

--- public/js/search/dometopia.js
+++ public/js/search/dometopia.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
     try {
@@ -29,17 +29,23 @@ const puppeteer = require('puppeteer');
             const productsArr = [];
 
             for (const productElement of productElements) {
-                const nameText = productElement.querySelector('dl > dd.goodsDisplayTitle > div > a > h6').textContent;
-                const name = nameText.replace(/\s+/g, '');
-                const priceText = productElement.querySelector('dl > dd.goodsDisplaySalePrice.clfix > div > table > tbody > tr > td.price_num').textContent;
-                const priceNumber = priceText.match(/\d+/g);
-                const price = parseInt(priceNumber.join(''), 10);
-                const imageText = productElement.querySelector('dl > dt > span > a > img');
-                const image = imageText.getAttribute('src');
-                const hrefText = productElement.querySelector('dl > dt > span > a').getAttribute('href');
-                const href = "https://dometopia.com" + hrefText;
-                const platform = '도매토피아';
-                productsArr.push({ name, price, image, href, platform });
+                const nameQuery = productElement.querySelector('dl > dd.goodsDisplayTitle > div > a > h6');
+                const priceQuery = productElement.querySelector('dl > dd.goodsDisplaySalePrice.clfix > div > table > tbody > tr > td.price_num');
+                const imageQuery = productElement.querySelector('dl > dt > span > a > img');
+                const hrefQuery = productElement.querySelector('dl > dt > span > a');
+                if (nameQuery != null && priceQuery != null && imageQuery != null && hrefQuery != null) {
+                    const nameText = productElement.querySelector('dl > dd.goodsDisplayTitle > div > a > h6').textContent;
+                    const name = nameText.replace(/\s+/g, '');
+                    const priceText = productElement.querySelector('dl > dd.goodsDisplaySalePrice.clfix > div > table > tbody > tr > td.price_num').textContent;
+                    const priceNumber = priceText.match(/\d+/g);
+                    const price = parseInt(priceNumber.join(''), 10);
+                    const imageText = productElement.querySelector('dl > dt > span > a > img');
+                    const image = imageText.getAttribute('src');
+                    const hrefText = productElement.querySelector('dl > dt > span > a').getAttribute('href');
+                    const href = "https://dometopia.com" + hrefText;
+                    const platform = '도매토피아';
+                    productsArr.push({ name, price, image, href, platform });
+                }
             }
 
             return productsArr;
