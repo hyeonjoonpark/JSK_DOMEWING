2e894f12046272dd3db72553d352d412fb6b1383
parkchef <parkchansu39@gmail.com>
2023-11-19 18:53:04 +0800

    commit

--- app/Http/Controllers/Admin/FormController.php
+++ app/Http/Controllers/Admin/FormController.php
@@ -496,121 +496,107 @@ class FormController extends Controller
         }
     }
 
-    public function domesin(Request $request, $username, $password, $categoryCode, $productImage, $descImage)
+    public function domesin($products)
     {
         try {
+            // 엑셀 파일 로드
             $spreadsheet = IOFactory::load(public_path('assets/excel/domesin.xls'));
-            $sheet = $spreadsheet->getsheet(0);
-            if ($request->shipping != '무료') {
-                $shipCost = 0;
-            } else {
-                $shipCost = $request->shipCost;
+            $sheet = $spreadsheet->getSheet(0);
+            // 데이터 추가
+            $rowIndex = 4;
+            foreach ($products as $product) {
+                $categoryId = $product->categoryId;
+                $categoryCode = DB::table('category')->where('id', $categoryId)->select('domesinCode')->first()->code;
+                $data = [
+                    '',
+                    $product->newProductName,
+                    $categoryCode,
+                    $product->newProductName,
+                    '기타',
+                    $product->productVendor,
+                    '',
+                    '',
+                    0,
+                    0,
+                    '',
+                    'N',
+                    $product->shippingCost,
+                    $product->shippingCost,
+                    '1508',
+                    $product->keywords,
+                    $product->productPrice,
+                    '',
+                    '',
+                    $product->productDetail,
+                    $product->newImageHref,
+                    '',
+                    '',
+                    '',
+                    '',
+                    '',
+                    0,
+                    '',
+                    '',
+                    0,
+                    '',
+                    '',
+                    1,
+                    '',
+                    0,
+                    1,
+                    '',
+                    '',
+                    35,
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                    '상품 상세설명 참조',
+                ];
+                // 엑셀에 데이터 추가
+                $colIndex = 1;
+                foreach ($data as $value) {
+                    $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, $value);
+                    $colIndex++;
+                }
+                $rowIndex++;
             }
-            $shipCost = $request->shipCost;
-            // 데이터 배열 생성
-            $domesinCode = DB::table('category')->where('code', $categoryCode)->select('domesinCode')->first();
-            $domesinCode = $domesinCode->domesinCode;
-            if ($request->taxability === '과세') {
-                $taxability = 0;
-            } else {
-                $taxability = 1;
-            }
-            if ($request->shipping == '선불') {
-                $shipping = 0;
-            } else if ($request->shipping == '착불') {
-                $shipping = 2;
-            } else if ($request->shipping == '무료') {
-                $shipping = 1;
-            }
-            if ($request->saleToMinor == '가능') {
-                $saleToMinor = '';
-            } else {
-                $saleToMinor = '1';
-            }
-            $dataset = [
-                'productCode' => '',
-                'productName' => $request->itemName,
-                'categoryCode' => $domesinCode,
-                'invoiceName' => $request->invoiceName,
-                'productCode2' => '',
-                'originated' => $request->origin,
-                'vendor' => $request->vendor,
-                'brand' => $request->vendor,
-                'model' => $request->model,
-                'taxability' => $taxability,
-                'shipType' => $shipping,
-                'bundledQuantity' => '',
-                'isInternationalShipping' => '',
-                'shipCost' => $shipCost,
-                'refundCost' => $shipCost,
-                'refundAddress' => '1508',
-                'keywords' => $request->keywords,
-                'productPrice' => $request->price,
-                'forcedPrice' => '',
-                'normalPrice' => '',
-                'descImage' => $descImage,
-                'productImage' => $productImage,
-                'extraImage1' => '',
-                'extraImage2' => '',
-                'extraImage3' => '',
-                'extraImage4' => '',
-                'extraImage5' => '',
-                'isExclusive' => '0',
-                'selectedOption' => '',
-                'inputOption' => '',
-                'credentials' => '0',
-                'credentialCode' => '',
-                'credentialNum' => '',
-                'isRefundable' => '1',
-                'saleToMinor' => $saleToMinor,
-                'isSale' => '0',
-                'isDisplay' => '1',
-                'productCondition' => '',
-                'productNotice' => '',
-                'productInformationCode' => $request->product_information,
-                'productInformation0' => '상품 상세설명에 표시',
-                'productInformation1' => '상품 상세설명에 표시',
-                'productInformation2' => '상품 상세설명에 표시',
-                'productInformation3' => '상품 상세설명에 표시',
-                'productInformation4' => '상품 상세설명에 표시',
-                'productInformation5' => '상품 상세설명에 표시',
-                'productInformation6' => '상품 상세설명에 표시',
-                'productInformation7' => '상품 상세설명에 표시',
-                'productInformation8' => '상품 상세설명에 표시',
-                'productInformation9' => '상품 상세설명에 표시',
-                'productInformation10' => '상품 상세설명에 표시',
-                'productInformation11' => '상품 상세설명에 표시',
-                'productInformation12' => '상품 상세설명에 표시',
-                'productInformation13' => '상품 상세설명에 표시',
-                'productInformation14' => '상품 상세설명에 표시',
-                'productInformation15' => '상품 상세설명에 표시',
-                'productInformation16' => '상품 상세설명에 표시',
-                'productInformation17' => '상품 상세설명에 표시',
-                'productInformation18' => '상품 상세설명에 표시',
-                'productInformation19' => '상품 상세설명에 표시',
-                'productInformation20' => '상품 상세설명에 표시',
-                'productInformation21' => '상품 상세설명에 표시',
-            ];
-            // 추가할 새로운 행의 위치를 지정합니다.
-            $newRow = 4; // 현재 데이터가 있는 가장 아래 행 다음에 추가하려면 +1을 사용합니다.
-            $col = 'A';
-            foreach ($dataset as $value) {
-                $sheet->setCellValue($col . $newRow, $value);
-                $col++;
-            }
-            // 엑셀 파일 업로드
-            // 변경된 내용을 파일로 저장
-            $writer = new Xls($spreadsheet);
-            $fileName = $username . '_' . date('YmdHis') . '.xls';
+            // 엑셀 파일 저장
+            $username = DB::table('users')
+                ->join('accounts', 'accounts.user_id', '=', 'users.id')
+                ->where('vendor_id', 6)
+                ->value('accounts.username');
+            $fileName = 'domesin_' . $username . '_' . now()->format('YmdHis') . '.xlsx';
             $formedExcelFile = public_path('assets/excel/formed/' . $fileName);
+            $writer = new Xls($spreadsheet);
             $writer->save($formedExcelFile);
-            $data['status'] = 1;
-            $data['return'] = $fileName;
-            return $data;
+            // 응답 데이터 반환
+            return [
+                'status' => 1,
+                'return' => $fileName,
+            ];
         } catch (Exception $e) {
-            $data['status'] = -1;
-            $data['return'] = $e->getMessage();
-            return $data;
+            return [
+                'status' => -1,
+                'return' => $e->getMessage(),
+            ];
         }
     }
     public function ownerclan($products, $userId)
