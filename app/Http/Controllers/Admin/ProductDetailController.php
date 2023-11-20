1a3eaaf30c8c07b974c42379037f188c43401b0c
parkchef <parkchansu39@gmail.com>
2023-11-20 17:54:35 +0800

    commit

--- app/Http/Controllers/Admin/ProductDetailController.php
+++ app/Http/Controllers/Admin/ProductDetailController.php
@@ -27,27 +27,49 @@ class ProductDetailController extends Controller
     {
         $products = $request->products;
         $processedProducts = [];
+        $maxRetries = 3; // 최대 재시도 횟수 설정
+
         foreach ($products as $product) {
-            $productDetail = "";
-            $vendor = DB::table('vendors')->where('name', $product['platform'])->select('name_eng')->first();
-            $href = $product['href'];
-            $script = public_path('js/details/' . $vendor->name_eng . '.js');
-            $command = "node " . escapeshellarg($script) . " " . escapeshellarg($href);
-            set_time_limit(0);
-            exec($command, $output, $returnCode);
-            $productDetail = json_decode($output[0], true);
-            $processedProduct = [
-                'name' => $product['name'],
-                'price' => $product['price'],
-                'href' => $product['href'],
-                'detail' => $productDetail['productDetail'],
-                'image' => $product['image'],
-                'platform' => $product['platform']
-            ];
-            $processedProducts[] = $processedProduct;
+            $retryCount = 0;
+
+            while ($retryCount < $maxRetries) {
+                $output = []; // 초기화
+                $vendor = DB::table('vendors')->where('name', $product['platform'])->select('name_eng')->first();
+                if (!$vendor) {
+                    // 벤더 정보가 없는 경우, 이번 상품 처리를 중단
+                    break;
+                }
+
+                $href = $product['href'];
+                $script = public_path('js/details/' . $vendor->name_eng . '.js');
+                $command = "node " . escapeshellarg($script) . " " . escapeshellarg($href);
+                set_time_limit(0);
+                exec($command, $output, $returnCode);
+
+                if ($returnCode == 0 && isset($output[0])) {
+                    // 성공적으로 처리된 경우, 상품 정보를 추가하고 반복문을 종료
+                    $productDetail = json_decode($output[0], true);
+                    $processedProduct = [
+                        'name' => $product['name'],
+                        'price' => $product['price'],
+                        'href' => $product['href'],
+                        'detail' => $productDetail['productDetail'],
+                        'image' => $product['image'],
+                        'platform' => $product['platform']
+                    ];
+                    $processedProducts[] = $processedProduct;
+                    break;
+                }
+
+                // 실패한 경우, 재시도 횟수를 증가시킴
+                $retryCount++;
+            }
         }
+
         return $this->getResponseData(1, $processedProducts);
     }
+
+
     protected function getResponseData($status, $return)
     {
         return ['status' => $status, 'return' => $return];
