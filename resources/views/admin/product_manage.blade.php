f54961cf857f58e3d18045c5652336e76435b841
parkchef <parkchansu39@gmail.com>
2023-11-21 09:44:40 +0800

    commit

--- resources/views/admin/product_manage.blade.php
+++ resources/views/admin/product_manage.blade.php
@@ -11,27 +11,59 @@
     <div class="row g-gs">
         <div class="col">
             <button class="btn btn-warning mb-5" onclick="productsUpload();">업로드 테스트</button>
-            <table id="productTable" class="datatable-init-export nowrap table" data-export-title="Export"
-                data-order='[[3, "asc"]]'>
+            <table class="datatable-init nk-tb-list nk-tb-ulist" data-auto-responsive="true" data-order='[[3, "asc"]]'>
                 <thead>
-                    <tr>
-                        <th>이미지</th>
-                        <th>상품명</th>
-                        <th>가격</th>
-                        <th>ACTION</th>
+                    <tr class="nk-tb-item nk-tb-head">
+                        <th class="nk-tb-col nk-tb-col-check">
+                            <div class="custom-control custom-control-sm custom-checkbox notext">
+                                <input type="checkbox" class="custom-control-input" id="products">
+                                <label class="custom-control-label" for="products"></label>
+                            </div>
+                        </th>
+                        <th class="nk-tb-col"><span class="sub-text">상품 정보</span></th>
+                        <th class="nk-tb-col tb-col-mb"><span class="sub-text">상품 가격</span></th>
+                        <th class="nk-tb-col tb-col-md"><span class="sub-text">업로드 일자</span></th>
+                        <th class="nk-tb-col tb-col-lg"><span class="sub-text">수정 일자</span></th>
+                        <th class="nk-tb-col nk-tb-col-tools text-end">ACTION
+                        </th>
                     </tr>
                 </thead>
                 <tbody id="productList">
                     @foreach ($products as $product)
-                        <tr>
-                            <td><img src="{{ $product->productImage }}" width="300" alt="상품 이미지"></td>
-                            <td>{{ $product->productName }}</td>
-                            <td>{{ $product->productPrice }}원</td>
-                            <td>
+                        <tr class="nk-tb-item">
+                            <td class="nk-tb-col nk-tb-col-check">
+                                <div class="custom-control custom-control-sm custom-checkbox notext">
+                                    <input type="checkbox" class="custom-control-input" id="product{{ $product->id }}">
+                                    <label class="custom-control-label" for="product{{ $product->id }}"></label>
+                                </div>
+                            </td>
+                            <td class="nk-tb-col col-4">
+                                <a href="{{ $product->productHref }}" target="_blank" class="user-card">
+                                    <div class="user-avatar bg-dim-primary d-none d-sm-flex">
+                                        <img src="{{ $product->productImage }}" alt="상품 대표 이미지">
+                                    </div>
+                                    <div class="user-info">
+                                        <span class="tb-lead">{{ $product->productName }} <span
+                                                class="dot dot-success d-md-none ms-1"></span></span>
+                                        <span>{{ $product->keywords }}</span>
+                                    </div>
+                                </a>
+                            </td>
+                            <td class="nk-tb-col" data-order="{{ $product->productPrice }}">
+                                <span class="tb-amount">{{ number_format($product->productPrice, 0) }} <span
+                                        class="currency">원</span></span>
+                            </td>
+                            <td class="nk-tb-col">
+                                <span>{{ $product->createdAt }}</span>
+                            </td>
+                            <td class="nk-tb-col" data-order="Email Verified - Kyc Unverified">
+                                <span>{{ $product->updatedAt }}</span>
+                            </td>
+                            <td class="nk-tb-col">
                                 <button class="btn btn-success">수정</button>
                                 <button class="btn btn-danger">삭제</button>
                             </td>
-                        </tr>
+                        </tr><!-- .nk-tb-item  -->
                     @endforeach
                 </tbody>
             </table>
