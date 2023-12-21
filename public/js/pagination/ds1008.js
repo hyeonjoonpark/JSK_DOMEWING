8f82183469d52c3fefeb7cfbbb269f1e1aadf230
parkchef <parkchansu39@gmail.com>
2023-12-21 19:33:29 +0900

    commit

--- public/js/pagination/ds1008.js
+++ public/js/pagination/ds1008.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: true, ignoreDefaultArgs: ['--enable-automation'] });
+    const browser = await puppeteer.launch({ headless: false, ignoreDefaultArgs: ['--enable-automation'] });
     const page = await browser.newPage();
     // page.setDefaultNavigationTimeout(0);
     try {
