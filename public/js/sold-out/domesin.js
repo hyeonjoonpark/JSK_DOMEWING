8bdeaec283351012550355bf57ad4c974bcb0d3e
parkchef <parkchansu39@gmail.com>
2023-12-28 17:17:43 +0900

    commit

--- public/js/sold-out/domesin.js
+++ public/js/sold-out/domesin.js
@@ -18,7 +18,7 @@ const puppeteer = require('puppeteer');
         await usernameInput.type(username);
         await passwordInput.type(password);
         await signInBtn.click(username);
-        await page.waitForNavigation();
+        await page.waitForNavigation({ timeout: 0 });
         await page.goto('https://www.domesin.com/scm/M_item/item_list.html?cate1=&cate2=&cate3=&cate4=&cid=&date=w&start_date=&end_date=&status=&raid=&i_type=&adult=&delivery_type=&isreturn=&tax=&item_sale_type=&ok=&is_overseas=&ls=&q_type=vender_code&rows=20&isort=iid&q=&q2=' + productCode, { waitUntil: 'networkidle2' });
         await page.click('#main > table.tb12 > tbody > tr:nth-child(2) > td:nth-child(1) > div:nth-child(2) > input');
         page.on('dialog', async dialog => {
