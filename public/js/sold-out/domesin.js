6a0e364d69a3c7de991bfad492f33a9087b7a814
parkchef <parkchansu39@gmail.com>
2023-12-28 17:52:36 +0900

    commit

--- public/js/sold-out/domesin.js
+++ public/js/sold-out/domesin.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
         ignoreDefaultArgs: ['--enable-automation']
     });
