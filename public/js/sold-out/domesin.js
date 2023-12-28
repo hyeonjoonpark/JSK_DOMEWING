074dec888e68fbb77b22dbd527c5c3968ce2cec5
parkchef <parkchansu39@gmail.com>
2023-12-28 17:22:35 +0900

    commit

--- public/js/sold-out/domesin.js
+++ public/js/sold-out/domesin.js
@@ -1,46 +1,53 @@
-const puppeteer = require('puppeteer');
+const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    ignoreDefaultArgs: ['--enable-automation']
+});
+const pages = await browser.pages();
+const page = pages[0];
+try {
+    const args = process.argv.slice(2);
+    const [username, password, productCode] = args;
+    // Navigate to login page with no timeout
+    await page.goto('https://www.domesin.com/scm/login.html', { timeout: 0 });
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-        ignoreDefaultArgs: ['--enable-automation']
+    // Wait for input fields and sign in button
+    const usernameInputSelector = 'body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > input';
+    const passwordInputSelector = 'body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td > input';
+    const signInBtnSelector = 'body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(3) > input[type=image]';
+
+    await page.waitForSelector(usernameInputSelector);
+    await page.waitForSelector(passwordInputSelector);
+    await page.waitForSelector(signInBtnSelector);
+
+    await page.type(usernameInputSelector, username);
+    await page.type(passwordInputSelector, password);
+    await page.click(signInBtnSelector);
+
+    // Wait for navigation after sign in with no timeout
+    await page.waitForNavigation({ timeout: 0 });
+    await page.goto('https://www.domesin.com/scm/M_item/item_list.html?cate1=&cate2=&cate3=&cate4=&cid=&date=w&start_date=&end_date=&status=&raid=&i_type=&adult=&delivery_type=&isreturn=&tax=&item_sale_type=&ok=&is_overseas=&ls=&q_type=vender_code&rows=20&isort=iid&q=&q2=' + productCode, { waitUntil: 'networkidle2' });
+    await page.click('#main > table.tb12 > tbody > tr:nth-child(2) > td:nth-child(1) > div:nth-child(2) > input');
+    page.on('dialog', async dialog => {
+        await dialog.accept();
+        return;
     });
-    const pages = await browser.pages();
-    const page = pages[0];
-    try {
-        const args = process.argv.slice(2);
-        const [username, password, productCode] = args;
-        await page.goto('https://www.domesin.com/scm/login.html', { timeout: 0 });
-        const usernameInput = await page.waitForSelector('body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > input');
-        const passwordInput = await page.waitForSelector('body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td > input');
-        const signInBtn = await page.waitForSelector('body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(3) > input[type=image]');
-        await usernameInput.type(username);
-        await passwordInput.type(password);
-        await signInBtn.click(username);
-        await page.waitForNavigation({ timeout: 0 });
-        await page.goto('https://www.domesin.com/scm/M_item/item_list.html?cate1=&cate2=&cate3=&cate4=&cid=&date=w&start_date=&end_date=&status=&raid=&i_type=&adult=&delivery_type=&isreturn=&tax=&item_sale_type=&ok=&is_overseas=&ls=&q_type=vender_code&rows=20&isort=iid&q=&q2=' + productCode, { waitUntil: 'networkidle2' });
-        await page.click('#main > table.tb12 > tbody > tr:nth-child(2) > td:nth-child(1) > div:nth-child(2) > input');
-        page.on('dialog', async dialog => {
-            await dialog.accept();
-            return;
-        });
-        const [newPage] = await Promise.all([
-            new Promise(resolve => browser.once('targetcreated', target => resolve(target.page()))),
-            page.click('#btn_total_sold')
-        ]);
+    const [newPage] = await Promise.all([
+        new Promise(resolve => browser.once('targetcreated', target => resolve(target.page()))),
+        page.click('#btn_total_sold')
+    ]);
 
-        // 새 페이지에서 원하는 요소의 textContent 가져오기
-        if (newPage) {
-            await newPage.waitForSelector('body > table > tbody > tr:nth-child(2) > td > div:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(4)');
-            const textContent = await newPage.$eval('body > table > tbody > tr:nth-child(2) > td > div:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(4)', element => element.textContent);
-            if (textContent.includes('변경')) {
-                console.log(true);
-            }
+    // 새 페이지에서 원하는 요소의 textContent 가져오기
+    if (newPage) {
+        await newPage.waitForSelector('body > table > tbody > tr:nth-child(2) > td > div:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(4)');
+        const textContent = await newPage.$eval('body > table > tbody > tr:nth-child(2) > td > div:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(4)', element => element.textContent);
+        if (textContent.includes('변경')) {
+            console.log(true);
         }
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        await browser.close();
     }
-})();
+} catch (error) {
+    console.error('Error:', error);
+} finally {
+    await browser.close();
+}
+}) ();
