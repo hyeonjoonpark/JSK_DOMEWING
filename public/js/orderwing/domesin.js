f95c72a782e9c979b540faabb0bce303d5610da5
parkchef <parkchansu39@gmail.com>
2024-01-02 11:34:13 +0800

    commit

--- public/js/orderwing/domesin.js
+++ public/js/orderwing/domesin.js
@@ -31,14 +31,14 @@ const path = require('path');
     try {
         const args = process.argv.slice(2);
         const [username, password] = args;
-        await page.goto('https://www.domesin.com/scm/login.html');
-        const usernameInput = await page.waitForSelector('body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td > input');
-        const passwordInput = await page.waitForSelector('body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td > input');
-        const signInBtn = await page.waitForSelector('body > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > table > tbody > tr > td:nth-child(3) > input[type=image]');
+        await page.goto('https://www.domesin.com/scm/login.html', { waitUntil: 'networkidle2', timeout: 0 });
+        const usernameInput = await page.waitForSelector('input[name="m_id"]');
+        const passwordInput = await page.waitForSelector('input[name="m_pw"]');
+        const signInBtn = await page.waitForSelector('input[type="image"]');
         await usernameInput.type(username);
         await passwordInput.type(password);
         await signInBtn.click();
-        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        await page.waitForNavigation({ timeout: 0 });
         await page.goto('http://domesin.com/scm/M_order/list.html', { waitUntil: 'networkidle2' });
         await page.click('#main > table.tb13 > tbody > tr:nth-child(1) > td:nth-child(1) > input[type=checkbox]');
         // 다운로드 경로 설정
@@ -50,6 +50,6 @@ const path = require('path');
     } catch (error) {
         console.error('Error:', error);
     } finally {
-        await browser.close();
+        // await browser.close();
     }
 })();
