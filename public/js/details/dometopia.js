3dc37f99db04569edffab3690c550294f3e39c2b
parkchef <85852697+parkchef@users.noreply.github.com>
2024-01-11 13:46:45 +0800

    commit

--- public/js/details/dometopia.js
+++ public/js/details/dometopia.js
@@ -1,55 +1,50 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
+const loginToSite = async (page, username, password) => {
+    await page.goto('https://dometopia.com/member/login', { waitUntil: 'networkidle2', timeout: 0 });
+    await page.type('#userid', username);
+    await page.type('#password', password);
+    await page.click('#doto_login > div.clearbox.mt20 > div.fleft > form > div > input.login-btn');
+    await page.waitForNavigation();
+};
 
+const getProductDetails = async (page, productHref) => {
+    await page.goto(productHref, { waitUntil: 'load', timeout: 0 });
+    return page.evaluate((productHref) => {
+        const baseURL = 'https://dometopia.com';
+        const productName = document.querySelector('#info > div.goods_info.clearbox > form > div.container > div > h2').textContent.trim();
+        let productPrice = document.querySelector('#info > div.goods_info.clearbox > form > div.container > table > tbody > tr:nth-child(2) > td > ul:nth-child(2) > li:nth-child(3) > span').textContent.trim();
+        productPrice = parseInt(productPrice.replace(/[^\d]/g, ''));
+
+        const productImages = document.querySelectorAll('#goods_thumbs > div.box > div.slides_container.hide img');
+        const productImage = productImages.length > 3 ? productImages[2].src : productImages[0]?.src;
+
+        const images = document.querySelectorAll('#detail > div > div.section.info > div.goods_description > div.detail-img img');
+        const productDetail = images.length === 0 ? [] : Array.from(images, img => {
+            let src = img.getAttribute('src');
+            return src.startsWith('http://') || src.startsWith('https://') ? src : new URL(src, baseURL).href;
+        });
+
+        return {
+            productName,
+            productPrice,
+            productImage,
+            productDetail,
+            hasOption: false,
+            productOptions: [],
+            productHref,
+            sellerID: 3
+        };
+    }, productHref);
+};
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
     try {
-        const args = process.argv.slice(2);
-        const [productHref, username, password] = args;
-        // Sign-in.
-        await page.goto('https://dometopia.com/member/login', { waitUntil: 'networkidle2', timeout: 0 });
-        await page.type('#userid', username);
-        await page.type('#password', password);
-        await page.click('#doto_login > div.clearbox.mt20 > div.fleft > form > div > input.login-btn');
-        await page.waitForNavigation();
-        await page.goto(productHref, { waitUntil: 'load', timeout: 0 });
-        const productContents = await page.evaluate((productHref) => {
-            const thirdImage = document.querySelector('#goods_thumbs > div.box_thumbs > ul > li:nth-child(3) > a');
-            if (thirdImage) {
-                thirdImage.click();
-            }
-            const baseURL = 'https://dometopia.com';
-            const productName = document.querySelector('#info > div.goods_info.clearbox > form > div.container > div > h2').textContent.trim();
-            let productPrice = document.querySelector('#info > div.goods_info.clearbox > form > div.container > table > tbody > tr:nth-child(2) > td > ul:nth-child(2) > li:nth-child(3) > span').textContent.trim();
-            // 가격에서 숫자만 추출
-            productPrice = productPrice.replace(/[^\d]/g, '');
-            productPrice = parseInt(productPrice);
-            const productImage = document.querySelector(`img[onerror="this.src='/data/skin/beauty/images/common/noimage.gif'"]`).src;
-            // 'detail' 아이디를 가진 요소 내의 모든 <img> 태그를 선택합니다.
-            const images = document.querySelectorAll('#detail > div > div.section.info > div.goods_description > div.detail-img img');
-            const productDetail = images.length === 0 ? [] : Array.from(images, img => {
-                let src = img.getAttribute('src');
-                // Check if the src is an absolute URL
-                if (src.startsWith('http://') || src.startsWith('https://')) {
-                    return src; // Return the src if it's already an absolute URL
-                } else {
-                    return new URL(src, baseURL).href; // Convert relative URL to absolute URL
-                }
-            });
-            let hasOption = false;
-            let productOptions = [];
-            return {
-                productName: productName,
-                productPrice: productPrice,
-                productImage: productImage,
-                productDetail: productDetail,
-                hasOption: hasOption,
-                productOptions: productOptions,
-                productHref: productHref,
-                sellerID: 3
-            };
-        }, productHref);
+        const [productHref, username, password] = process.argv.slice(2);
+        await loginToSite(page, username, password);
+        const productContents = await getProductDetails(page, productHref);
         console.log(JSON.stringify(productContents));
     } catch (error) {
         console.error('Error occurred:', error);
--- public/js/mining/dometopia.js
+++ public/js/mining/dometopia.js
@@ -1,66 +1,75 @@
 const puppeteer = require('puppeteer');
+
+async function login(page, username, password) {
+    await page.goto('https://dometopia.com/member/login', { waitUntil: 'networkidle2', timeout: 0 });
+    await page.type('#userid', username);
+    await page.type('#password', password);
+    await page.click('#doto_login > div.clearbox.mt20 > div.fleft > form > div > input.login-btn');
+    await page.waitForNavigation();
+}
+
 async function getProductList(page) {
     return page.evaluate(() => {
         const productSelector = 'td[valign="top"]';
         const baseURL = 'https://dometopia.com';
         const products = [];
 
-        const getProductDetails = productElement => {
-            try {
-                const nameSelector = 'dl > dd.goodsDisplayTitle > div > a > h6';
-                const priceSelector = 'dl > dd.goodsDisplaySalePrice.clfix > div > table > tbody > tr > td.price_num';
-                const imageSelector = 'dl > dt > span > a > img';
-                const hrefSelector = 'dl > dd.goodsDisplayTitle > div > a';
+        const isValidProduct = productElement => {
+            const singleTagSelector = 'dl > dd.goodsDisplayCode > table > tbody > tr:nth-child(1) > td > a:nth-child(1) > img';
+            const importTagSelector = 'dl > dd.goodsDisplayCode > table > tbody > tr:nth-child(1) > td > a:nth-child(1) > img:nth-child(3)';
+            const singleTagExists = productElement.querySelector(singleTagSelector) !== null;
+            const importTagExists = productElement.querySelector(importTagSelector) !== null;
 
-                const name = productElement.querySelector(nameSelector).textContent.trim();
-                const productPriceText = productElement.querySelector(priceSelector).textContent;
-                const image = productElement.querySelector(imageSelector).getAttribute('src');
-                const productHref = productElement.querySelector(hrefSelector).getAttribute('href');
+            return singleTagExists && !importTagExists;
+        };
 
-                if (!name.includes('해외직구')) {
-                    const price = parseInt(productPriceText.replace(/[^0-9]/g, '').trim());
-                    const href = baseURL + productHref;
-                    const platform = '도매토피아';
+        const extractProductDetails = productElement => {
+            const nameSelector = 'dl > dd.goodsDisplayTitle > div > a > h6';
+            const priceSelector = 'dl > dd.goodsDisplaySalePrice.clfix > div > table > tbody > tr > td.price_num';
+            const imageSelector = 'dl > dt > span > a > img';
+            const hrefSelector = 'dl > dd.goodsDisplayTitle > div > a';
 
-                    return { name, price, image, href, platform };
-                }
-            } catch (error) {
-                console.error('Error extracting product details:', error);
+            const name = productElement.querySelector(nameSelector)?.textContent.trim();
+            const productPriceText = productElement.querySelector(priceSelector)?.textContent;
+            const image = productElement.querySelector(imageSelector)?.getAttribute('src');
+            const productHref = productElement.querySelector(hrefSelector)?.getAttribute('href');
+
+            if (name && !name.includes('해외직구') && !name.includes('해외배송')) {
+                const price = parseInt(productPriceText.replace(/[^0-9]/g, '').trim());
+                const href = baseURL + productHref;
+                const platform = '도매토피아';
+
+                return { name, price, image, href, platform };
             }
         };
 
         document.querySelectorAll(productSelector).forEach(productElement => {
-            const productDetails = getProductDetails(productElement);
-            if (productDetails) {
-                products.push(productDetails);
+            if (isValidProduct(productElement)) {
+                const productDetails = extractProductDetails(productElement);
+                if (productDetails) {
+                    products.push(productDetails);
+                }
             }
         });
 
         return products;
     });
 }
+
 (async () => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     try {
-        const args = process.argv.slice(2);
-        const [listURL, username, password, curPage] = args;
-        // Sign-in.
-        await page.goto('https://dometopia.com/member/login', { waitUntil: 'networkidle2', timeout: 0 });
-        await page.type('#userid', username);
-        await page.type('#password', password);
-        await page.click('#doto_login > div.clearbox.mt20 > div.fleft > form > div > input.login-btn');
-        await page.waitForNavigation();
-        // Generate the full URL.
-        const params = '&perpage=150&page=' + curPage;
-        const fullURL = listURL + params;
-        // Move to the URL page.
+        const [listURL, username, password, curPage] = process.argv.slice(2);
+        await login(page, username, password);
+
+        const fullURL = `${listURL}&perpage=150&page=${curPage}`;
         await page.goto(fullURL, { waitUntil: 'networkidle2', timeout: 0 });
-        // Get the products in the list.
+
         const products = await getProductList(page);
         console.log(JSON.stringify(products));
     } catch (error) {
-        console.error(error);
+        console.error('Error occurred:', error);
     } finally {
         await browser.close();
     }
