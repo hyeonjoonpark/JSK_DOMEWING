bfa152b49ebeb8434002c1ad4a4014588124cbe7
parkchef <85852697+parkchef@users.noreply.github.com>
2024-01-15 16:12:44 +0800

    Update dometopia.js

--- public/js/mining/dometopia.js
+++ public/js/mining/dometopia.js
@@ -12,7 +12,11 @@ async function getProductList(page) {
         const baseURL = 'https://dometopia.com';
         const products = [];
         const isValidProduct = productElement => {
-            const soldOutTagExists = document.querySelector('#designDisplay_65a4e7f8f2ad8 > table > tbody > tr:nth-child(9) > td:nth-child(3) > dl > dd.goodsDisplayCode > table > tbody > tr:nth-child(1) > td > a:nth-child(2) > img');
+            const soldOutElement = document.querySelector('#designDisplay_65a4e7f8f2ad8 > table > tbody > tr:nth-child(9) > td:nth-child(3) > dl > dd.goodsDisplayCode > table > tbody > tr:nth-child(1) > td > a:nth-child(2) > img');
+            let soldOutTagExists = false;
+            if (soldOutElement) {
+                soldOutTagExists = true;
+            }
             const tagElements = Array.from(productElement.querySelectorAll('dl > dd.goodsDisplayCode > table > tbody > tr:nth-child(1) > td > a:nth-child(1) img'));
             const singleTagSRC = '/data/skin/beauty/images/icon/G.gif';
             const importTagSRC = '/data/skin/beauty/images/icon/__H.gif';
