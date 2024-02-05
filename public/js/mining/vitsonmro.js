030f1170835ae9ba192bea84b1e0e23866efa40e
parkchef <85852697+parkchef@users.noreply.github.com>
2024-02-05 13:03:27 +0800

    commit

--- public/js/mining/vitsonmro.js
+++ public/js/mining/vitsonmro.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: true, ignoreDefaultArgs: ['--enable-automation'] });
+    const browser = await puppeteer.launch({ headless: false, ignoreDefaultArgs: ['--enable-automation'] });
     const page = await browser.newPage();
     try {
         const args = process.argv.slice(2);
         const [listURL, username, password, curPage] = args;
         await signIn(page, username, password);
         await selectNumPages(page, listURL);
-        await moveToPage(page, listURL, curPage);
+        await moveToPage(page, curPage);
         const products = await scrapeProducts(page);
         console.log(JSON.stringify(products));
     } catch (error) {
@@ -28,13 +28,13 @@ async function signIn(page, username, password) {
 async function selectNumPages(page, listURL) {
     await page.goto(listURL, { waitUntil: 'networkidle2', timeout: 0 });
     await page.select('#grid > div.k-pager-wrap.k-grid-pager.k-widget.k-floatwrap > span.k-pager-sizes.k-label > span > select', '60');
-    await new Promise((page) => setTimeout(page, 10000));
+    await new Promise((page) => setTimeout(page, 5000));
 }
-async function moveToPage(page, listURL, curPage) {
+async function moveToPage(page, curPage) {
     curPage = parseInt(curPage);
-    if (curPage != 1) {
+    if (curPage !== 1) {
         await page.evaluate((curPage) => {
-            const pageBtn = document.querySelector('a[class="k-link"][data-page="' + curPage + '"]');
+            const pageBtn = document.querySelector('a[class="k-link"][data-page="2"]');
             pageBtn.setAttribute('data-page', curPage);
             pageBtn.click();
         }, curPage);
@@ -44,9 +44,6 @@ async function moveToPage(page, listURL, curPage) {
 async function scrapeProducts(page) {
     const products = await page.evaluate(() => {
         function processProduct(productElement) {
-            // Remove this.
-            const index = parseInt(productElement.querySelector('td:nth-child(3)').textContent.trim());
-            //
             const stockText = productElement.querySelector('td:nth-child(9) > span.hdsp_bot').textContent.trim();
             if (stockText !== '재고보유') {
                 return false;
@@ -60,7 +57,7 @@ async function scrapeProducts(page) {
             const productCode = productElement.querySelector('td:nth-child(5) > span.hdsp_top').textContent.replace(/[^0-9]/g, '').trim();
             const href = 'https://vitsonmro.com/mro/shop/productDetail.do?productCode=' + productCode;
             const platform = '비츠온엠알오';
-            return { name, price, image, href, platform, index };
+            return { name, price, image, href, platform };
         }
         const productElements = document.querySelectorAll('#grid > div.k-grid-content.k-auto-scrollable > table > tbody tr');
         const products = [];
--- public/js/pagination/vitsonmro.js
+++ public/js/pagination/vitsonmro.js
@@ -1,17 +1,17 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     try {
         const args = process.argv.slice(2);
         const [listURL] = args;
         await page.goto(listURL, { waitUntil: 'networkidle2', timeout: 0 });
-        const numPages = await page.evaluate(() => {
-            const numPagesText = document.querySelector('body > div.container > div > div.content > div.top_toolbar.align_side > div.tool_left > p > strong').textContent.trim();
-            const numPages = parseInt(numPagesText.replace(/[^\d]/g, ''));
-            return numPages;
+        const numProducts = await page.evaluate(() => {
+            const numProductsText = document.querySelector('body > div.container > div > div.content > div.top_toolbar.align_side > div.tool_left > p > strong').textContent.trim();
+            const numProducts = parseInt(numProductsText.replace(/[^\d]/g, ''));
+            return numProducts;
         });
-        console.log(numPages);
+        console.log(numProducts);
     } catch (error) {
         console.error(error);
     } finally {
