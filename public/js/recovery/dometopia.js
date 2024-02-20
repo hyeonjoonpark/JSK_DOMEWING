a4b420d9402ce6e1be71aa87379fde5c915534ac
parkchef <85852697+parkchef@users.noreply.github.com>
2024-02-20 15:58:42 +0900

    Update dometopia.js

--- public/js/recovery/dometopia.js
+++ public/js/recovery/dometopia.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 const getProductDetails = async (page, productHref) => {
     await page.goto(productHref, { waitUntil: 'domcontentloaded' });
