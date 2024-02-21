6eeed4344025fbfcaf45945559c00cb78b74394f
parkchef <85852697+parkchef@users.noreply.github.com>
2024-02-21 13:56:13 +0900

    commit

--- public/js/details/vitsonmro.js
+++ public/js/details/vitsonmro.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: true, ignoreDefaultArgs: ['--enable-automation'] });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     try {
         const args = process.argv.slice(2);
