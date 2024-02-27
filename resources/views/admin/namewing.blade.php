f1f9360b2292c662e4a333e3849c4c17f344f233
parkchef <85852697+parkchef@users.noreply.github.com>
2024-02-27 15:27:22 +0900

    commit

--- resources/views/admin/namewing.blade.php
+++ resources/views/admin/namewing.blade.php
@@ -32,8 +32,55 @@
             </div>
         @endforelse
     </div>
-    @section('sold_out_modal')
-    @endsection
+    <div class="modal" tabindex="-1" role="dialog" id="selectB2bModal" data-bs-backdrop="static"
+        data-bs-keyboard="false">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">B2B 업체 선택</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="" class="form-label">B2B 업체 리스트</label>
+                                <div class="row">
+                                    <div class="col-6 mb-3">
+                                        <div class="custom-control custom-checkbox">
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox" id="sellwing" name="sellwing" value="0"
+                                                    class="custom-control-input" checked>
+                                                <label class="custom-control-label" for="sellwing">셀윙</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @foreach ($b2bs as $b2b)
+                                        <div class="col-6 mb-3">
+                                            <div class="custom-control custom-checkbox">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" id="b2b{{ $b2b->vendor_id }}" name="b2bs"
+                                                        value="{{ $b2b->vendor_id }}" class="custom-control-input" checked>
+                                                    <label class="custom-control-label"
+                                                        for="b2b{{ $b2b->vendor_id }}">{{ $b2b->name }}</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" id="runSoldOutBtn">선택완료</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">종료하기</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 {{-- 추가 스크립트 섹션 --}}
 @section('scripts')
@@ -41,6 +88,7 @@
         var rememberToken = '{{ Auth::user()->remember_token }}';
 
         function initEditProductName(productCode) {
+            popupLoader(1, '변경된 상품명을 DB에 적용 중입니다.');
             const newProductName = $('#' + productCode).val();
             $.ajax({
                 url: '/api/product/edit-name',
@@ -52,6 +100,7 @@
                     rememberToken
                 },
                 success: function(response) {
+                    closePopup();
                     const status = response.status;
                     let statusStr = 'error';
                     if (status === true) {
@@ -60,12 +109,11 @@
                     swalWithReload(response.return, statusStr);
                 },
                 error: function(response) {
+                    closePopup();
                     console.log(response);
                     swalError('통신 상에 문제가 발생했습니다.');
                 }
             });
         }
     </script>
-@endsection
-@section('sold_out_scripts')
 @endsection
--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -121,4 +121,45 @@
         audioError.play();
         swalError('"API 통신 요청 과정에서 에러가 발생했습니다."');
     }
+
+    function initSoldOut(productCodes) {
+        $('#runSoldOutBtn').off('click').on('click', function() {
+            runSoldOut(productCodes);
+        });
+        $('#selectB2bModal').modal('show');
+    }
+
+    function runSoldOut(productCodes) {
+        closePopup();
+        popupLoader(0, '"선택된 업체들에게 품절 소식을 알리고 올게요."');
+        const b2bs = $('input[name="b2bs"]:checked').map(function() {
+            return $(this).val();
+        }).get();
+        const isSellwingChecked = $('#sellwing').prop('checked');
+        console.log(isSellwingChecked);
+        $.ajax({
+            url: '/api/product/sold-out',
+            type: 'POST',
+            dataType: 'JSON',
+            data: {
+                productCodes,
+                rememberToken,
+                b2bs,
+                isSellwingChecked
+            },
+            success: soldOutSuccess,
+            error: AjaxErrorHandling
+        });
+    }
+
+    function soldOutSuccess(response) {
+        console.log(response);
+        closePopup();
+        const status = response.status;
+        if (status === true) {
+            swalSuccess(response.return);
+        } else {
+            swalError(response.return);
+        }
+    }
 </script>
