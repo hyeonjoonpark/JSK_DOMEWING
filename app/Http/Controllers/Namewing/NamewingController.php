19e5627d92922ef883bdfc41d86608802384bed6
parkchef <85852697+parkchef@users.noreply.github.com>
2024-02-28 16:36:16 +0900

    commit

--- app/Http/Controllers/Namewing/NamewingController.php
+++ app/Http/Controllers/Namewing/NamewingController.php
@@ -20,21 +20,37 @@ class NamewingController extends Controller
             ->where('pr.is_active', 'Y')
             ->where('v.is_active', 'ACTIVE')
             ->get();
-        // 중복된 productName을 가진 상품 찾기
+
+        // 중복된 productName의 총 종류 수 찾기
+        $totalDuplicateGroups = DB::table('minewing_products')
+            ->select('productName')
+            ->where('isActive', 'Y')
+            ->groupBy('productName')
+            ->havingRaw('COUNT(*) > 1')
+            ->get()
+            ->count(); // 중복 그룹의 총 개수를 계산
+
+        // 첫 번째 중복된 productName을 가진 상품 찾기
         $duplicates = DB::table('minewing_products')
             ->select('productName', DB::raw('COUNT(*) as count'))
             ->where('isActive', 'Y')
             ->groupBy('productName')
             ->havingRaw('COUNT(*) > 1')
             ->first();
-        // 중복된 productName을 가진 상품들의 상세 정보 가져오기
-        $products = DB::table('minewing_products')
-            ->where('isActive', 'Y')
-            ->where('productName', $duplicates->productName)
-            ->get(['productName', 'productHref', 'productImage', 'productCode', 'productPrice']);
+
+        // 중복된 productName을 가진 상품들의 상세 정보 가져오기 (첫 번째 중복 그룹에 대해서만)
+        $products = [];
+        if ($duplicates) {
+            $products = DB::table('minewing_products')
+                ->where('isActive', 'Y')
+                ->where('productName', $duplicates->productName)
+                ->get(['productName', 'productHref', 'productImage', 'productCode', 'productPrice']);
+        }
+
         return view('admin/namewing', [
             'duplicatedProducts' => $products,
-            'b2bs' => $b2bs
+            'b2bs' => $b2bs,
+            'totalDuplicateGroups' => $totalDuplicateGroups // 중복 그룹의 총 개수 전달
         ]);
     }
 }
