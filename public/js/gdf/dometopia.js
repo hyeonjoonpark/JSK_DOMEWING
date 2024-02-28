808ec2d45e18ada48fbe3cd7ccca1b306d3fb8b5
parkchef <85852697+parkchef@users.noreply.github.com>
2024-02-28 14:47:12 +0900

    Update dometopia.js

--- public/js/gdf/dometopia.js
+++ public/js/gdf/dometopia.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     try {
         const startUrl = 'https://dometopia.com/goods/search?search_text=GDF&page=';
