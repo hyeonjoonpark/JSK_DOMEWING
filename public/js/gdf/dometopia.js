61800330fd6f912d4454d4182348ed56d9c51415
parkchef <85852697+parkchef@users.noreply.github.com>
2024-02-28 14:01:37 +0900

    Update dometopia.js

--- public/js/gdf/dometopia.js
+++ public/js/gdf/dometopia.js
@@ -1,14 +1,15 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     try {
         const startUrl = 'https://dometopia.com/goods/search?search_text=GDF&page=';
         const productHrefs = [];
-        for (let i = 54; i > 0; i--) {
+        for (let i = 2; i > 0; i--) {
             const fullUrl = startUrl + i;
             await page.goto(fullUrl, 'domcontentloaded');
-            productHrefs.push(...await getProductHrefs(page));
+            const tmpProductHrefs = await getProductHrefs(page);
+            productHrefs.push(...tmpProductHrefs);
         }
         console.log(JSON.stringify(productHrefs));
     } catch (error) {
@@ -18,14 +19,5 @@ const puppeteer = require('puppeteer');
     }
 })();
 async function getProductHrefs(page) {
-    const productHrefs = await page.evaluate(() => {
-        const productElements = document.querySelectorAll('td[valign="top"][width="216px"][style="position: relative;"]');
-        const productHrefs = [];
-        for (const productElement of productElements) {
-            const productHref = productElement.querySelector('dl > dd.goodsDisplayTitle > div > a').textContent.trim();
-            productHrefs.push(productHref);
-        }
-        return productHrefs;
-    });
-    return productHrefs;
+    return page.evaluate(() => Array.from(document.querySelectorAll('td[valign="top"] a'), element => element.href));
 }
