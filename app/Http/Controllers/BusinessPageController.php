7663706720c3798f9a7eb0064dc1330046d34445
VingKong <vingkongchong@gmail.com>
2024-03-03 16:50:45 +0800

    update business page

--- app/Http/Controllers/BusinessPageController.php
+++ app/Http/Controllers/BusinessPageController.php
@@ -4,37 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class BusinessPageController extends Controller
 {
     public function showBusinessPage(Request $request)
     {
         $partners = $this->getAllPartner();
-        return view('business_page.index', ['partners' => $partners]);
+        $testimonials = $this->getAllTestimonials();
+        return view('business_page.index', ['partners' => $partners, 'testimonials' => $testimonials]);
     }
 
     public function getAllPartner()
     {
-        $images = [
-            'https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg',
-            'https://images.pexels.com/photos/19840881/pexels-photo-19840881/free-photo-of-take-me-to-the-stars-if-you-like-my-work-consider-supporting-me-at-https-www-patreon-com-marekpiwnicki.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/20071905/pexels-photo-20071905/free-photo-of-moon-cake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg',
-            'https://images.pexels.com/photos/19840881/pexels-photo-19840881/free-photo-of-take-me-to-the-stars-if-you-like-my-work-consider-supporting-me-at-https-www-patreon-com-marekpiwnicki.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/20071905/pexels-photo-20071905/free-photo-of-moon-cake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg',
-            'https://images.pexels.com/photos/19840881/pexels-photo-19840881/free-photo-of-take-me-to-the-stars-if-you-like-my-work-consider-supporting-me-at-https-www-patreon-com-marekpiwnicki.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/20071905/pexels-photo-20071905/free-photo-of-moon-cake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg',
-            'https://images.pexels.com/photos/19840881/pexels-photo-19840881/free-photo-of-take-me-to-the-stars-if-you-like-my-work-consider-supporting-me-at-https-www-patreon-com-marekpiwnicki.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/20071905/pexels-photo-20071905/free-photo-of-moon-cake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg',
-            'https://images.pexels.com/photos/19840881/pexels-photo-19840881/free-photo-of-take-me-to-the-stars-if-you-like-my-work-consider-supporting-me-at-https-www-patreon-com-marekpiwnicki.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/20071905/pexels-photo-20071905/free-photo-of-moon-cake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg',
-            'https://images.pexels.com/photos/19840881/pexels-photo-19840881/free-photo-of-take-me-to-the-stars-if-you-like-my-work-consider-supporting-me-at-https-www-patreon-com-marekpiwnicki.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            'https://images.pexels.com/photos/20071905/pexels-photo-20071905/free-photo-of-moon-cake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        ];
+        $images = DB::table('partnership')->where('status', 'Y')->inRandomOrder()->get();
         return $images;
+    }
+
+    public function getAllTestimonials()
+    {
+        $testimonials = DB::table('testimonial')->where('status', 'Y')->get();
+
+        foreach ($testimonials as $record) {
+            $dateTime = new DateTime($record->created_at);
+
+            // Format the date
+            $record->formatted_date = $dateTime->format('Y년 m월 d일, ');
+            $record->formatted_date .= ($dateTime->format('A') === 'AM' ? '오전' : '오후') . $dateTime->format('h시 i분');
+        }
+
+        return $testimonials;
     }
 }
