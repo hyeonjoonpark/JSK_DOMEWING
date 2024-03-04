31b7d6422c66b2ba061a7660d78c5c1687e4416d
parkchef <85852697+parkchef@users.noreply.github.com>
2024-03-04 22:46:46 +0900

    commit

--- app/Http/Controllers/Gdf/VitsonMroController.php
+++ app/Http/Controllers/Gdf/VitsonMroController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers\Gdf;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Product\ProcessController;
+use App\Http\Controllers\Productwing\SoldOutController;
 use Illuminate\Support\Facades\DB;
 
 class VitsonMroController extends Controller
 {
     private $processController;
+    private $soldOutController;
+    private $gdfController;
     const VENDOR_ID = 13;
     const USER_ID = 15;
     public function __construct()
     {
         $this->processController = new ProcessController();
+        $this->soldOutController = new SoldOutController();
+        $this->gdfController = new GdfController();
     }
     public function main()
     {
@@ -24,12 +29,30 @@ class VitsonMroController extends Controller
         if ($trackOverAmountProducts === false) {
             return false;
         }
-        return $trackOverAmountProducts;
+        $productCodes = $trackOverAmountProducts;
+        $productCodesJsonFile = storage_path('app/public/gdf/' . uniqid() . '.json');
+        file_put_contents($productCodesJsonFile, json_encode($productCodes));
+        $b2bs = DB::table('vendors')
+            ->where('is_active', 'Y')
+            ->get();
+        foreach ($productCodes as $productCode) {
+            foreach ($b2bs as $b2b) {
+                $b2bId = $b2b->id;
+                $vendorEngName = $b2b->name_eng;
+                $account = $this->processController->getAccount(self::USER_ID, $b2bId);
+                $username = $account->username;
+                $password = $account->password;
+                $this->soldOutController->sendSoldOutRequest($productCode, $vendorEngName, $username, $password);
+            }
+        }
+        unlink($tempFilePath);
+        return $this->gdfController->inactiveProducts($productCodes);
     }
     private function getVitsonMroProducts()
     {
         return DB::table('minewing_products')
             ->where('sellerID', self::VENDOR_ID)
+            ->where('isActive', 'Y')
             ->get(['productCode', 'productHref']);
     }
     private function genHrefsJsonFile($products)
@@ -44,7 +67,7 @@ class VitsonMroController extends Controller
         $command = "node {$scriptPath} {$tempFilePath}";
         exec($command, $output, $resultCode);
         if ($resultCode === 0 && isset($output[0])) {
-            $productCodes = $output[0];
+            $productCodes = json_decode($output[0], true);
             return $productCodes;
         }
         return false;
--- public/js/gdf/vitsonmro.js
+++ public/js/gdf/vitsonmro.js
@@ -1,19 +1,41 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     try {
         const args = process.argv.slice(2);
         const [tempFilePath] = args;
         const products = JSON.parse(fs.readFileSync(tempFilePath, 'utf8'));
+        const productCodes = [];
         for (const product of products) {
             const productCode = product.productCode;
             const productHref = product.productHref;
+            const isOverAmountResult = await isOverAmount(page, productHref);
+            if (isOverAmountResult) {
+                productCodes.push(productCode);
+            }
         }
+        console.log(JSON.stringify(productCodes));
     } catch (error) {
         console.error('Error occurred:', error);
     } finally {
         await browser.close();
     }
 })();
+async function isOverAmount(page, productHref) {
+    await page.goto(productHref, { waitUntil: 'domcontentloaded' });
+    const isOverAmount = await page.evaluate(() => {
+        try {
+            const amountText = document.querySelector('#eachAmount').textContent.trim();
+            const amount = parseInt(amountText.replace(/[^\d]/g, ''), 10);
+            if (amount > 1) {
+                return true;
+            }
+            return false;
+        } catch (error) {
+            return true;
+        }
+    });
+    return isOverAmount;
+}
