2855e975e26af3baa6048c3b2e0073178343b943
shoojoy <157701033+shoojoy@users.noreply.github.com>
2024-03-13 18:51:04 +0900

    Update gtgb2b.js

--- public/js/minewing/details/gtgb2b.js
+++ public/js/minewing/details/gtgb2b.js
@@ -5,11 +5,11 @@ const fs = require('fs');
     const page = await browser.newPage();
     try {
         const args = process.argv.slice(2);
-        // const [tempFilePath, username, password] = args;
-        // const urls = JSON.parse(fs.readFileSync(tempFilePath, 'utf8'));
-        const urls = ['https://www.gtgb2b.com/goods/goods_view.php?goodsNo=987'];
-        const username = 'sungil2018';
-        const password = 'tjddlf88!@';
+        const [tempFilePath, username, password] = args;
+        const urls = JSON.parse(fs.readFileSync(tempFilePath, 'utf8'));
+        // const urls = ['https://www.gtgb2b.com/goods/goods_view.php?goodsNo=987'];
+        // const username = 'sungil2018';
+        // const password = 'tjddlf88!@';
         await login(page, username, password);
         const products = [];
         for (const url of urls) {
@@ -27,14 +27,10 @@ const fs = require('fs');
     }
 })();
 async function login(page, username, password) {
-    // 로그인 페이지로 이동합니다.
     await page.goto('https://gtgb2b.com/member/login.php', { waitUntil: 'networkidle0' });
-    // 사용자 이름과 비밀번호 입력 필드에 값을 입력합니다.
     await page.type('#loginId', username);
     await page.type('#loginPwd', password);
-    // 로그인 버튼을 클릭합니다.
     await page.click('#formLogin > div.login > button');
-    // 페이지 내비게이션이 완료될 때까지 기다립니다.
     await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 }
 async function scrapeProduct(page, productHref) {
@@ -57,7 +53,7 @@ async function scrapeProduct(page, productHref) {
             const productPrice = parseInt(productPriceText.replace(/[^\d]/g, ''));
             const productImage = document.querySelector('#mainImage > img').src;
             // 상세 이미지
-            const productDetailElements = document.querySelectorAll('#contents img');
+            const productDetailElements = document.querySelectorAll('#detail > div.txt-manual > div:nth-child(12) > img');
             const productDetail = [];
             for (const productDetailElement of productDetailElements) {
                 const productDetailImage = productDetailElement.src
@@ -71,7 +67,7 @@ async function scrapeProduct(page, productHref) {
                 hasOption: hasOption,
                 productOptions: productOptions,
                 productHref: productHref,
-                sellerID: 20
+                sellerID: 29
             };
         }, productHref, productOptions, hasOption);
         return product;
@@ -79,82 +75,73 @@ async function scrapeProduct(page, productHref) {
         return false;
     }
 }
-async function getProductOptions(page) {
-    // Retrieve all select elements.
-    const selectElements = await page.$$('select[name="opt[]"]');
+// async function getProductOptions(page) {
 
-    // Initialize an array to hold all product options.
-    let productOptions = [];
+//     const selectElements = await page.$$('select[name="opt[]"]');
 
-    // Ensure that there are at least two select elements.
-    if (selectElements.length > 1) {
-        // Retrieve values and texts of options from the first select element.
-        const firstOptionValues = await selectElements[0].evaluate(select =>
-            Array.from(select.options)
-                .filter(option => option.value.trim() !== '')
-                .map(option => ({ value: option.value, text: option.textContent.trim() }))
-        );
+//     let productOptions = [];
 
-        // Loop through each option value of the first select element.
-        for (const { value: firstOptionValue, text: firstOptionText } of firstOptionValues) {
-            // Select the option in the first select element.
-            await selectElements[0].select(firstOptionValue);
+//     if (selectElements.length > 1) {
 
-            // Wait for any dynamic content to load.
-            await new Promise((page) => setTimeout(page, 1000));
+//         const firstOptionValues = await selectElements[0].evaluate(select =>
+//             Array.from(select.options)
+//                 .filter(option => option.value.trim() !== '')
+//                 .map(option => ({ value: option.value, text: option.textContent.trim() }))
+//         );
 
-            // Evaluate the second select element after selecting an option in the first.
-            const secondSelectOptions = await selectElements[1].evaluate(select =>
-                Array.from(select.options)
-                    .filter(option => option.value.trim() !== '') // 빈 값이 아닌 모든 옵션을 포함
-                    .map(option => {
-                        const text = option.textContent.trim();
-                        let name = text, price = 0; // 기본적으로 가격을 0으로 설정
-                        // 가격 정보가 있는 경우, 이름과 가격을 분리
-                        if (text.includes('(')) {
-                            const parts = text.split('(');
-                            name = parts[0].trim();
-                            const pricePart = parts[1];
-                            if (pricePart && pricePart.includes('원')) {
-                                price = parseInt(pricePart.replace(/[^\d-+]/g, ''), 10);
-                            }
-                        }
-                        return { name, price };
-                    })
-            );
+//         for (const { value: firstOptionValue, text: firstOptionText } of firstOptionValues) {
+//             await selectElements[0].select(firstOptionValue);
+//             await new Promise((page) => setTimeout(page, 1000));
 
-            // Combine options from the first and second select elements and add them to the productOptions array.
-            secondSelectOptions.forEach(({ name: secondOptionName, price }) => {
-                productOptions.push({
-                    optionName: `${firstOptionText} ${secondOptionName}`,
-                    optionPrice: price,
-                });
-            });
-        }
-    } else {
-        productOptions = await selectElements[0].evaluate(select =>
-            Array.from(select.options)
-                .filter(option => option.value.trim() !== '') // 빈 값이 아닌 모든 옵션을 포함
-                .map(option => {
-                    const text = option.textContent.trim();
-                    let optionName = text, optionPrice = 0; // 기본적으로 가격을 0으로 설정
-                    // 가격 정보가 있는 경우, 이름과 가격을 분리
-                    if (text.includes('(')) {
-                        const parts = text.split('(');
-                        optionName = parts[0].trim();
-                        const pricePart = parts[1];
-                        if (pricePart && pricePart.includes('원')) {
-                            optionPrice = parseInt(pricePart.replace(/[^\d-+]/g, ''), 10);
-                        }
-                    }
-                    return { optionName, optionPrice };
-                })
-        );
-    }
+//             const secondSelectOptions = await selectElements[1].evaluate(select =>
+//                 Array.from(select.options)
+//                     .filter(option => option.value.trim() !== '') // 빈 값이 아닌 모든 옵션을 포함
+//                     .map(option => {
+//                         const text = option.textContent.trim();
+//                         let name = text, price = 0; // 기본적으로 가격을 0으로 설정
+//                         // 가격 정보가 있는 경우, 이름과 가격을 분리
+//                         if (text.includes('(')) {
+//                             const parts = text.split('(');
+//                             name = parts[0].trim();
+//                             const pricePart = parts[1];
+//                             if (pricePart && pricePart.includes('원')) {
+//                                 price = parseInt(pricePart.replace(/[^\d-+]/g, ''), 10);
+//                             }
+//                         }
+//                         return { name, price };
+//                     })
+//             );
+//             secondSelectOptions.forEach(({ name: secondOptionName, price }) => {
+//                 productOptions.push({
+//                     optionName: `${firstOptionText} ${secondOptionName}`,
+//                     optionPrice: price,
+//                 });
+//             });
+//         }
+//     } else {
+//         productOptions = await selectElements[0].evaluate(select =>
+//             Array.from(select.options)
+//                 .filter(option => option.value.trim() !== '') // 빈 값이 아닌 모든 옵션을 포함
+//                 .map(option => {
+//                     const text = option.textContent.trim();
+//                     let optionName = text, optionPrice = 0; // 기본적으로 가격을 0으로 설정
+//                     // 가격 정보가 있는 경우, 이름과 가격을 분리
+//                     if (text.includes('(')) {
+//                         const parts = text.split('(');
+//                         optionName = parts[0].trim();
+//                         const pricePart = parts[1];
+//                         if (pricePart && pricePart.includes('원')) {
+//                             optionPrice = parseInt(pricePart.replace(/[^\d-+]/g, ''), 10);
+//                         }
+//                     }
+//                     return { optionName, optionPrice };
+//                 })
+//         );
+//     }
 
-    return productOptions;
-}
-async function getHasOption(page) {
-    // Directly return the evaluation result.
-    return page.evaluate(() => document.querySelectorAll('select[name="opt[]"]').length > 0);
-}
+//     return productOptions;
+// }
+// async function getHasOption(page) {
+//     // Directly return the evaluation result.
+//     return page.evaluate(() => document.querySelectorAll('select[name="opt[]"]').length > 0);
+// }
