ae56e60a5a6f88dcb0246d9e4b238d8e91e09f90
parkchef_ <parkchansu39@gmail.com>
2024-03-13 11:57:16 +0900

    commit

--- public/js/minewing/details/janghak.js
+++ public/js/minewing/details/janghak.js
@@ -8,15 +8,14 @@ const fs = require('fs'); // 파일 시스템 모듈을 불러옵니다.
         const args = process.argv.slice(2); // 커맨드 라인 인자를 가져옵니다.
         const [tempFilePath, username, password] = args; // 인자에서 파일 경로, 사용자 이름, 비밀번호를 추출합니다.
         const urls = JSON.parse(fs.readFileSync(tempFilePath, 'utf8')); // URL 목록이 담긴 파일을 읽어서 파싱합니다.
-
+        // const urls = ['https://www.jhmungu.com/shop/goods_detail.php?ps_uid=157686'];
+        // const username = "jskorea2023";
+        // const password = "Tjddlf88!@#";
         await signIn(page, username, password); // 로그인 함수를 호출합니다.
 
         const products = []; // 스크래핑된 상품 정보를 저장할 배열을 초기화합니다.
         for (const url of urls) { // URL 목록을 순회합니다.
-            const navigateWithRetryResult = await navigateWithRetry(page, url); // 페이지 이동 시도합니다. 실패하면 다음 URL로 넘어갑니다.
-            if (navigateWithRetryResult === false) {
-                continue;
-            }
+            await page.goto(url, { waitUntil: 'domcontentloaded' });
             const product = await scrapeProduct(page, url); // 현재 URL에서 상품 정보를 스크래핑합니다.
             if (product === false) { // 스크래핑이 실패하면 다음 URL로 넘어갑니다.
                 continue;
@@ -28,21 +27,8 @@ const fs = require('fs'); // 파일 시스템 모듈을 불러옵니다.
         console.error('Error occurred:', error); // 오류 발생 시 오류 메시지를 출력합니다.
     } finally {
         await browser.close(); // 작업이 끝나면 브라우저를 닫습니다.
-    }
+    } ``
 })();
-async function navigateWithRetry(page, url, attempts = 3, delay = 2000) {
-    for (let i = 0; i < attempts; i++) {
-        try {
-            await page.goto(url, { waitUntil: 'domcontentloaded' });
-            return true;
-        } catch (error) {
-            if (i < attempts - 1) {
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }
-        }
-    }
-    return false;
-}
 async function signIn(page, username, password) {
     await page.goto('https://www.jhmungu.com/shop/login.php', { waitUntil: 'networkidle0' });
     await page.type('#memberLogin > div > div:nth-child(1) > form > div:nth-child(2) > input[type=text]:nth-child(1)', username);
@@ -52,14 +38,20 @@ async function signIn(page, username, password) {
 }
 
 async function scrapeProduct(page, productHref) {
-
-    await new Promise(resolve => setTimeout(resolve, 1000)); // 페이지 로드 후 1초 대기
     const product = await page.evaluate((productHref) => {
-        const regex = /\([^()]*\)/g;
-        const productNameText = document.querySelector('body > div.container.goods_detail_skin > div.g_skin_head.mt-lg-3.mb-3.mb-md-4.row.px-2.px-lg-0 > div.goods_info.col-12.col-lg.pl-lg-3 > h5 > span').textContent.trim();
-        const productName = productNameText.replace(regex, '');
-
-        const productPrice = document.querySelector('body > div.container.goods_detail_skin > div.g_skin_head.mt-lg-3.mb-3.mb-md-4.row.px-2.px-lg-0 > div.goods_info.col-12.col-lg.pl-lg-3 > div.row.align-items-center.detail-price-info.justify-content-end > div.col-md.col-7 > div > div:nth-child(2) > span > span').textContent.trim().replace(/[^\d]/g, '');
+        const tagList = document.querySelectorAll('body > div.container.goods_detail_skin > div.g_skin_head.mt-lg-3.mb-3.mb-md-4.row.px-2.px-lg-0 > div.goods_info.col-12.col-lg.pl-lg-3 > div.d-flex.flex-wrap.justify-content-between.mb-3.pb-2.border-bottom > div.col-auto.d-flex > p span');
+        if (tagList) {
+            for (const tag of tagList) {
+                const tagText = tag.textContent.trim();
+                if (tagText.includes('배송불가') || tagText.includes('안함') || tagText.includes('품절') || tagText.includes('미정') || tagText.includes('단종') || tagText.includes('반품불가')) {
+                    return false;
+                }
+            }
+        }
+        // 상품명
+        const productName = document.querySelector('body > div.container.goods_detail_skin > div.g_skin_head.mt-lg-3.mb-3.mb-md-4.row.px-2.px-lg-0 > div.goods_info.col-12.col-lg.pl-lg-3 > h5 > span').textContent.trim();
+        // 상품 가격
+        const productPrice = document.querySelector('body > div.container.goods_detail_skin > div.g_skin_head.mt-lg-3.mb-3.mb-md-4.row.px-2.px-lg-0 > div.goods_info.col-12.col-lg.pl-lg-3 > div.row.align-items-center.detail-price-info.justify-content-end > div.col-md.col-7 > div > div:nth-child(2) > span').textContent.trim().replace(/[^\d]/g, '');
 
         const productImageElement = document.querySelector('#goodsDetailImage').getAttribute('src').trim();
         const productImage = productImageElement.startsWith('http') ? productImageElement : `https:${productImageElement}`;
@@ -75,6 +67,9 @@ async function scrapeProduct(page, productHref) {
         const productDetailImageElements = document.querySelectorAll('#menu_explain > div:nth-child(3) img');
         const excludedPaths = ['/web/img/start', '/web/img/event'];
         const productDetail = getAbsoluteImageUrls(productDetailImageElements, baseUrl, ...excludedPaths);
+        if (productDetail.length < 1) {
+            productDetail.push(productImage);
+        }
 
         let hasOption = false;
         let productOptions = [];
