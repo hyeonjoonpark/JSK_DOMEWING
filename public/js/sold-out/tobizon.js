c39b17d64860bfdbe2ec09048119881e8737a438
sangxxjin <sj_0368@naver.com>
2024-03-26 14:48:53 +0900

    refactor tobizon

--- public/js/sold-out/tobizon.js
+++ public/js/sold-out/tobizon.js
@@ -1,40 +1,24 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+
+    clearPopup(page);
     try {
         const [username, password, tempFilePath] = process.argv.slice(2);
         const productCodes = JSON.parse(fs.readFileSync(tempFilePath, 'utf8'));
-        page.on('dialog', async dialog => {
-            await dialog.accept();
-            return;
-        });
         const searchStr = productCodes.join(',');
 
         await login(page, username, password);
+        await processPageList(page, searchStr);
+        await doSoldOut(page);
 
-        await page.goto('http://www.tobizon.co.kr/scm/goods/goods_list.php', { waitUntil: 'networkidle0' });
-        await page.select('select[name="search_key"]', 'vgoodscd');
-        await delay(1000);
-        await page.select('select[name="listsize"]', '500');
-        await page.type('#area_search_str', searchStr);
-        await new Promise((page) => setTimeout(page, 1000));
-        await page.click('#searchFrm > tbody > tr:nth-child(1) > td:nth-child(2) > button');
-        await new Promise((page) => setTimeout(page, 1000));
-        const productElement = await page.$$('#loadWarpGoodslist > table > tbody > tr');
-        if (productElement.length < 2) {
-            console.log(false);
-            return;
-        }
-        await new Promise((page) => setTimeout(page, 1000));
-        await page.evaluate(() => {
-            const inputElement = document.querySelector('#chkAll');
-            inputElement?.click();
-        });
-        await new Promise((page) => setTimeout(page, 1000));
-        await page.click('#loadWarpGoodslist > div:nth-child(2) > div > div > button.button.warning.xs');
-        console.log(true);
     } catch (error) {
         console.error('Error:', error);
     } finally {
@@ -44,10 +28,54 @@ const fs = require('fs');
 
 const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
 
-const login = async (page, username, password) => {
+async function login(page, username, password) {
     await page.goto('http://www.tobizon.co.kr/mall/member/login.php', { waitUntil: 'networkidle0' });
     await page.type('#mid', username);
     await page.type('#password', password);
     await page.click('#sfrm > div.col-xs-6 > div:nth-child(1) > div.pull-right > button.primary-btn.primary');
     await page.waitForNavigation({ waitUntil: 'load' });
 };
+
+async function processPageList(page, searchStr) {
+    await page.goto('http://www.tobizon.co.kr/scm/goods/goods_list.php', { waitUntil: 'networkidle0' });
+    await page.select('select[name="search_key"]', 'vgoodscd');
+    await delay(1000);
+    await page.select('select[name="listsize"]', '500');
+    await page.type('#area_search_str', searchStr);
+    await new Promise((page) => setTimeout(page, 1000));
+    await page.click('#searchFrm > tbody > tr:nth-child(1) > td:nth-child(2) > button');
+    await new Promise((page) => setTimeout(page, 1000));
+}
+
+async function doSoldOut(page) {
+    const productElement = await page.$$('#loadWarpGoodslist > table > tbody > tr');
+    if (productElement.length < 2) {
+        console.log(false);
+        return;
+    }
+    await new Promise((page) => setTimeout(page, 1000));
+    await page.evaluate(() => {
+        const inputElement = document.querySelector('#chkAll');
+        inputElement?.click();
+    });
+    await new Promise((page) => setTimeout(page, 1000));
+    await page.click('#loadWarpGoodslist > div:nth-child(2) > div > div > button.button.warning.xs');
+    console.log(true);
+    return;
+}
+
+
+async function clearPopup(page) {
+    page.on('dialog', async dialog => {
+        const message = dialog.message();
+        if (message.includes('품절 처리')) {
+            await dialog.accept();
+            console.log(true);
+        }
+        else {
+            await dialog.dismiss();
+            console.log(false);
+        }
+        return;
+    });
+}
