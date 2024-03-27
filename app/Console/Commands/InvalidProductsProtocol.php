dadb946191383091aa87b3e57939c246356e32e9
parkchef_ <parkchansu39@gmail.com>
2024-03-27 18:19:51 +0900

    commit

---
+++ app/Console/Commands/InvalidProductsProtocol.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class InvalidProductsProtocol extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:invalid-products-protocol {vendorId}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $vendorId = $this->argument('vendorId');
+        $vendor = $this->getVendor($vendorId);
+        $account = $this->getAccount($vendorId);
+        $products = $this->getProducts($vendorId);
+        $mappedProducts = array_map(function ($product) {
+            return [
+                'productCode' => $product->productCode,
+                'productHref' => $product->productHref
+            ];
+        }, $products);
+        $productsChunks = array_chunk($mappedProducts, 100);
+        $invalidProductCodes = [];
+        foreach ($productsChunks as $productsChunk) {
+            $productsChunkFilePath = $this->createProductsChunkFile($productsChunk);
+            $trackProductsResult = $this->trackProducts($productsChunkFilePath, $vendor->name_eng);
+            if ($trackProductsResult === false) {
+                return $productsChunkFilePath;
+            } else {
+                foreach ($trackProductsResult as $productCode) {
+                    $invalidProductCodes[] = $productCode;
+                }
+            }
+        }
+        return $this->createResultFile($invalidProductCodes);
+    }
+    private function createResultFile($invalidProductCodes)
+    {
+        $directoryPath = storage_path('app/public/invalid-products-protocol/results/');
+        if (!is_dir($directoryPath)) {
+            mkdir($directoryPath, 0777, true);
+        }
+        $filePath = $directoryPath . Str::uuid() . '.json';
+        file_put_contents($filePath, json_encode($invalidProductCodes));
+        return true;
+    }
+    private function trackProducts($productsChunkFilePath, $vendorEngName)
+    {
+        $directoryPath = public_path('js/gdf/');
+        $scriptPath = $directoryPath . $vendorEngName . '.js';
+        $command = "node {$scriptPath} {$productsChunkFilePath}";
+        exec($command, $output, $resultCode);
+        if ($resultCode === 0 && isset($output[0])) {
+            $productCodes = json_decode($output[0]);
+            return $productCodes;
+        }
+        return false;
+    }
+    private function createProductsChunkFile($productsChunk)
+    {
+        $directoryPath = storage_path('app/public/invalid-products-protocol/targets/');
+        if (!is_dir($directoryPath)) {
+            mkdir($directoryPath, 0777, true);
+        }
+        $productsChunkFilePath = $directoryPath . Str::uuid() . '.json';
+        file_put_contents($productsChunkFilePath, json_encode($productsChunk));
+        return $productsChunkFilePath;
+    }
+    private function getVendor($vendorId)
+    {
+        return DB::table('product_search AS ps')
+            ->join('vendors AS v', 'v.id', '=', 'ps.vendor_id')
+            ->where('v.id', $vendorId)
+            ->first();
+    }
+    private function getProducts($vendorId)
+    {
+        return DB::table('minewing_products')
+            ->where('isActive', 'Y')
+            ->where('sellerID', $vendorId)
+            ->get(['productCode', 'productHref'])
+            ->toArray();
+    }
+    private function getAccount($vendorId)
+    {
+        return DB::table('accounts')
+            ->where('vendor_id', $vendorId)
+            ->first();
+    }
+}
---
+++ public/js/gdf/janghak.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    try {
+        const args = process.argv.slice(2);
+        const [tempFilePath] = args;
+        const products = JSON.parse(fs.readFileSync(tempFilePath, 'utf8'));
+        const invalidProducts = [];
+        for (const product of products) {
+            const { productCode, productHref } = product;
+            const isValid = await validateProduct(page, productHref);
+            if (isValid === false) {
+                invalidProducts.push(productCode);
+            }
+        }
+        console.log(JSON.stringify(invalidProducts));
+    } catch (error) {
+        console.error('Error occurred:', error);
+    } finally {
+        await browser.close();
+    }
+})();
+async function validateProduct(page, url) {
+    await page.goto(url, { waitUntil: 'load' });
+    const isValid = await page.evaluate(() => {
+        const tagElement = document.querySelector('span.bg-noretn');
+        if (tagElement) {
+            const tagText = tagElement.textContent.trim();
+            if (tagText.includes('온라인 판매금지')) {
+                return false;
+            }
+        }
+        return true;
+    });
+    return isValid;
+}
