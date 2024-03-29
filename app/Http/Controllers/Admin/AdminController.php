4e39285a9e99fcc3ed47baede5c5484f43afab90
sangxxjin <sj_0368@naver.com>
2024-03-29 11:59:38 +0900

    refactor except funn

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -24,6 +24,7 @@ class AdminController extends Controller
             ->join('vendors AS v', 'ps.vendor_id', '=', 'v.id')
             ->where('v.is_active', 'ACTIVE')
             ->where('ps.is_active', 'Y')
+            ->whereNotIn('v.id', [33])
             ->get();
         $vendorHrefs = array_map(function ($vendor) {
             return $vendor->vendor_href;
