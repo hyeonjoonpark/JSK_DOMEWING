233d49234b965156053a288dfb52d01ece8fd129
shoojoy <157701033+shoojoy@users.noreply.github.com>
2024-04-09 19:05:57 +0900

    commit

--- public/js/minewing/details/2bpet.js
+++ public/js/minewing/details/2bpet.js
@@ -2,12 +2,15 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const { timeout } = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     try {
         const args = process.argv.slice(2);
-        const [tempFilePath, username, password] = args;
-        const urls = JSON.parse(fs.readFileSync(tempFilePath, 'utf8'));
+        // const [tempFilePath, username, password] = args;
+        // const urls = JSON.parse(fs.readFileSync(tempFilePath, 'utf8'));
+        const urls = ['https://www.2bpet.co.kr/product/content.asp?guid=207126&cate=14423&params='];
+        const username = "jskorea2024";
+        const password = "tjddlf88!@";
         await signIn(page, username, password);
         const products = [];
         for (const url of urls) {
