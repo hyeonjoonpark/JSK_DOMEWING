c8197802d8ee30cd31b6843f4671c4bcb56bb2f2
parkchef_ <parkchansu39@gmail.com>
2024-04-16 10:19:21 +0900

    Update cheonyu.js

--- public/js/minewing/cheonyu.js
+++ public/js/minewing/cheonyu.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080 });
     try {
