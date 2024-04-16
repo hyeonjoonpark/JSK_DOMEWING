e6da21857d10957bd7c412b0ef8a4a7151bc25b5
parkchef_ <parkchansu39@gmail.com>
2024-04-16 10:14:32 +0900

    commit

--- app/Console/Commands/Store11stCategories.php
+++ app/Console/Commands/Store11stCategories.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
@@ -15,13 +14,17 @@ class Store11stCategories extends Command
 
     public function handle()
     {
-        $url = "http://api.11st.co.kr/rest/cateservice/category";
+        $url = 'http://api.11st.co.kr/rest/cateservice/category'; // Use an environment variable
         $response = Http::get($url);
 
         if ($response->successful()) {
-            $categories = $this->parseCategories($response->body());
-            $this->insertCategories($categories);
-            $this->info('Categories have been successfully fetched and inserted.');
+            try {
+                $categories = $this->parseCategories($response->body());
+                $this->insertCategories($categories);
+                $this->info('Categories have been successfully fetched and inserted.');
+            } catch (\Exception $e) {
+                $this->error("Parsing or insertion failed: " . $e->getMessage());
+            }
         } else {
             $this->error("Failed to fetch categories: " . $response->status());
         }
@@ -39,24 +42,24 @@ class Store11stCategories extends Command
     {
         foreach ($xmlElement->category as $category) {
             $newPath = $path . ($path ? ' > ' : '') . $category->dispNm;
-            if ($category->leafYn == 'Y') {
+            if ($category->leafYn == 'Y') { // 최하위 노드만 추가
                 $categories[] = [
                     'name' => $newPath,
                     'code' => (string)$category->dispNo
                 ];
             } else {
-                $this->buildCategoryPath($category, $categories, $newPath);
+                $this->buildCategoryPath($category, $categories, $newPath); // 최하위가 아니면 계속 순회
             }
         }
     }
 
     private function insertCategories($categories)
     {
-        foreach ($categories as $category) {
-            DB::table('11st_category')->updateOrInsert(
-                ['code' => $category['code']],
-                ['name' => $category['name']]
-            );
+        try {
+            print_r($categories);
+            DB::table('st11_category')->upsert($categories, ['code'], ['name']); // Use upsert for performance improvement
+        } catch (\Exception $e) {
+            echo $e->getMessage();
         }
     }
 }
