02b9aae6ebfc7cc6a61b5f451f96f3d11f4c5ad5
parkchef_ <parkchansu39@gmail.com>
2024-04-26 13:20:34 +0900

    commit

--- app/Http/Controllers/Partners/OrderController.php
+++ app/Http/Controllers/Partners/OrderController.php
@@ -12,18 +12,13 @@ use Illuminate\Support\Facades\Request;
 class OrderController extends Controller
 {
     private $ssac;
-    private $account;
-    private $contentType;
-
     public function __construct()
     {
         $this->ssac = new SmartStoreApiController();
-        $this->account = $this->getAccount();
-        $this->contentType = 'application/json';
     }
     public function index()
     {
-        $orderList = $this->getOrderList($this->ssac, $this->account, $this->contentType);
+        $orderList = $this->getOrderList();
         $orderIds = $this->getOrderIds($orderList);
 
 
@@ -36,12 +31,10 @@ class OrderController extends Controller
             'orderDetails' => $orderDetails
         ]);
     }
-    public function getOrderList($ssac, $account, $contentType)
+    public function getOrderList()
     {
-        $ssac = new SmartStoreApiController();
-        $account = DB::table('smart_store_accounts')
-            ->where('partner_id', Auth::guard('partner')->id())
-            ->first();
+
+        $account = $this->getAccount();
         $contentType = 'application/json';
         $method = 'GET';
         $url = 'https://api.commerce.naver.com/external/v1/pay-order/seller/product-orders/last-changed-statuses';
@@ -49,7 +42,7 @@ class OrderController extends Controller
             'lastChangedFrom' => Carbon::now()->subDays(1)->format('Y-m-d\TH:i:s.vP'),
             'lastChangedTo' => Carbon::now()->subDays(0)->format('Y-m-d\TH:i:s.vP'),
         ];
-        $response = $ssac->builder($account, $contentType, $method, $url, $data);
+        $response = $this->ssac->builder($account, $contentType, $method, $url, $data);
         return $response;
     }
     private function getOrderIds($request)
@@ -66,28 +59,24 @@ class OrderController extends Controller
     }
     public function smart_storeDetail()
     {
-        $ssac = new SmartStoreApiController();
-        $account = DB::table('smart_store_accounts')
-            ->where('partner_id', Auth::guard('partner')->id())
-            ->first();
+
+        $account = $this->getAccount();
         $contentType = 'application/json';
         $method = 'GET';
         $url = 'https://api.commerce.naver.com/external/v1/pay-order/seller/orders/2024042534417071/product-order-ids';
         $data = [];
-        $response = $ssac->builder($account, $contentType, $method, $url, $data);
+        $response = $this->ssac->builder($account, $contentType, $method, $url, $data);
         return $response;
     }
     public function getOrderDetails($productOrderIds)
     {
-        $ssac = new SmartStoreApiController();
-        $account = DB::table('smart_store_accounts')
-            ->where('partner_id', Auth::guard('partner')->id())
-            ->first();
+
+        $account = $this->getAccount();
         $contentType = 'application/json';
         $method = 'POST';
         $url = 'https://api.commerce.naver.com/external/v1/pay-order/seller/product-orders/query';
         $data = ['productOrderIds' => $productOrderIds];
-        $response = $ssac->builder($account, $contentType, $method, $url, $data);
+        $response = $this->ssac->builder($account, $contentType, $method, $url, $data);
         return $response;
     }
 
