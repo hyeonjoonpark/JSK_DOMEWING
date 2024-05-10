9bd2a98f187c195ab54b1f74477e7ed203102a56
parkchef_ <parkchansu39@gmail.com>
2024-05-10 16:09:14 +0900

    commit

--- app/Console/Commands/ProductDetailRecovery.php
+++ app/Console/Commands/ProductDetailRecovery.php
@@ -6,6 +6,7 @@ use DOMDocument;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProductDetailRecovery extends Command
 {
@@ -28,129 +29,42 @@ class ProductDetailRecovery extends Command
      */
     public function handle()
     {
-        $productCodeFilePath = storage_path('app/public/product-codes/product_detail_recovery.json');
-        $productCodeFile = file_get_contents($productCodeFilePath);
+        $products = $this->getTargetProducts();
+        print_r($this->processProducts($products));
+    }
+    protected function scrapeProductDetail($filePath)
+    {
+        $scriptPath = public_path('js/detail-recovery/ds1008.js');
+        $account = DB::table('accounts')
+            ->where('vendor_id', 14)
+            ->first(['username', 'password']);
+        $username = $account->username;
+        $password = $account->password;
+        $command = "node {$scriptPath} {$filePath} {$username} $password";
+        exec($command, $output, $resultCode);
+        if ($resultCode === 0 && isset($output[0])) {
+            $products = json_decode($output[0], true);
+        }
+    }
+    protected function processProducts($products)
+    {
+        $chunkedProducts = array_chunk($products, 100, false);
+        foreach ($chunkedProducts as $products) {
+            $tempProductCodeFilePath = public_path('js/detail-recovery/ds1008_targets.json');
+            file_put_contents($tempProductCodeFilePath, json_encode($products));
+            $this->scrapeProductDetail($tempProductCodeFilePath);
+            unlink($tempProductCodeFilePath);
+        }
+    }
+    protected function getTargetProducts()
+    {
+        $productCodeFile = file_get_contents(storage_path('app/public/product-codes/product_detail_recovery.json'));
         $productCodes = json_decode($productCodeFile);
         $products = DB::table('minewing_products')
             ->whereIn('productCode', $productCodes)
-            // ->where('productDetail', 'like', '%옵션%')
-            ->where('sellerID', 14)
-            ->limit(3)
-            ->get(['productCode', 'productHref', 'productDetail']);
-        echo (count($products));
-        // return;
-        $hrefs = [];
-        foreach ($products as $product) {
-            $hrefs[] = [
-                'href' => $product->productHref,
-                'code' => $product->productCode  // 제품 코드도 저장
-            ];
-        }
-
-        // JSON 데이터를 임시 파일에 저장
-        $tempFilePath = tempnam(sys_get_temp_dir(), 'hrefs_');
-        file_put_contents($tempFilePath, json_encode($hrefs));
-        // Node.js 스크립트 실행, 파일 경로를 인자로 전달
-        // $command = "node public/js/detail-recovery/vitsonmro.js $tempFilePath jskorea2023 Tjddlf88!@#"; //비츠온엠알오 13
-        $command = "node public/js/detail-recovery/ds1008.js $tempFilePath sungil2018 tjddlf88!@#"; //씨오코리아 14
-        // $command = "node public/js/detail-recovery/cheonyu.js $tempFilePath jskorea2023 Tjddlf88!@"; //천유닷컴 50
-        exec($command, $output, $return_var);
-
-        // 결과 출력
-        foreach ($output as $productInfoJson) {
-            $productInfos = json_decode($productInfoJson, true);  // JSON 문자열을 배열로 변환
-            foreach ($productInfos as $productInfo) {  // 모든 제품 정보를 반복 처리
-                if (isset($productInfo['productDetail']) && is_array($productInfo['productDetail'])) {
-                    $originDetailOption = DB::table('minewing_products')
-                        ->where('productCode', $productInfo['productCode'])
-                        ->get('productDetail');
-
-                    $productDetailUrls = $productInfo['productDetail'];  // 상세 이미지 URL 배열
-                    $optionName = $this->processProductOption($originDetailOption);  // 옵션 이름 추출, 첫 번째 이미지 URL을 사용
-                    $realUrl = $this->saveProductImages($productDetailUrls, $productInfo['productCode']); //이미지 저장
-                    $finalHtml = $this->processProductDetail($realUrl, $optionName);  // 최종 HTML 생성
-
-
-                    echo $finalHtml;
-                    DB::table('minewing_products')
-                        ->where('productCode', $productInfo['productCode'])
-                        ->update(['productDetail' => $finalHtml]);
-                }
-            }
-        }
-        // 임시 파일 삭제
-        unlink($tempFilePath);
-    }
-    protected function saveProductImages(array $productDetailImages, $productCode)
-    {
-        $baseDir = "public/images/CDN/detail";
-        if (!file_exists($baseDir)) {
-            mkdir($baseDir, 0777, true);
-        }
-        $localImagePaths = [];
-        foreach ($productDetailImages as $index => $imageUrl) {
-            $encodedUrl = $this->encodeImageUrl($imageUrl);
-            $imageContent = file_get_contents($encodedUrl);
-            $filename = basename($imageUrl);  // URL에서 파일명 추출
-            $filePath = "$baseDir/$filename";  // 저장할 파일 경로
-            file_put_contents($filePath, $imageContent);
-            echo "Image saved to: $filePath\n";
-            $localImagePaths[] = $filePath;  // 로컬 저장 경로 추가
-        }
-        return $localImagePaths;
-    }
-    protected function encodeImageUrl($url)
-    {
-        // URL의 구성 요소를 분해
-        $parts = parse_url($url);
-
-        // 경로 부분에 있는 특수 문자 인코딩
-        if (isset($parts['path'])) {
-            $parts['path'] = implode("/", array_map("rawurlencode", explode("/", $parts['path'])));
-        }
-
-        // 쿼리 스트링 처리
-        if (isset($parts['query'])) {
-            parse_str($parts['query'], $query);
-            $parts['query'] = http_build_query($query);
-        }
-
-        // URL 재구성
-        return $parts['scheme'] . '://' . $parts['host'] . (isset($parts['path']) ? $parts['path'] : '') .
-            (isset($parts['query']) ? '?' . $parts['query'] : '');
-    }
-    protected function processProductDetail(array $productImagePaths, string $optionName): string
-    {
-        $html = $optionName ? "<h1 style='color:red; font-weight:bold; font-size:4rem;'>$optionName</h1><br><br><br>" : '';
-        $html .= "<center>";
-        foreach ($productImagePaths as $imagePath) {
-            $html .= "<img src='$imagePath'><br>";  // 저장된 이미지의 로컬 경로를 src에 사용
-        }
-        $html .= "</center>";
-        return $html;
-    }
-
-    function decodeUnicodeEscapeSequence($str)
-    {
-        // 유니코드 이스케이프 시퀀스를 UTF-8 문자열로 변환
-        return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
-            return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
-        }, $str);
-    }
-
-    protected function processProductOption($originProductDetail)
-    {
-        $doc = new DOMDocument();
-        libxml_use_internal_errors(true);
-        $doc->loadHTML(mb_convert_encoding($originProductDetail, 'HTML-ENTITIES', 'UTF-8'));
-        libxml_clear_errors();
-        $optionName = "";
-        $h1Tags = $doc->getElementsByTagName('h1');
-        if ($h1Tags->length > 0) {
-            $optionName = $h1Tags->item(0)->textContent;
-            // 유니코드 이스케이프 시퀀스 디코딩
-            $optionName = $this->decodeUnicodeEscapeSequence($optionName);
-        }
-        return $optionName;
+            ->where('sellerID', 14) // 씨오코리아
+            ->get(['productDetail', 'productHref', 'id'])
+            ->toArray();
+        return $products;
     }
 }
--- public/js/detail-recovery/ds1008.js
+++ public/js/detail-recovery/ds1008.js
@@ -8,7 +8,7 @@ const fs = require('fs');
     try {
         const args = process.argv.slice(2);
         const [filePath, username, password] = args;
-        await page.goto('https://www.ds1008.com/member/login.php', { waitUntil: 'networkidle2' });
+        await page.goto('https://www.ds1008.com/member/login.php', { waitUntil: 'networkidle0' });
         await page.type('#loginId', username);
         await page.type('#loginPwd', password);
         await page.click('#formLogin > div.login > button');
@@ -18,29 +18,30 @@ const fs = require('fs');
         const items = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
         for (const item of items) {
-            const { href, code } = item;  // href와 code 추출
-            await page.goto(href, { waitUntil: 'networkidle2' });
-            const productContents = await page.evaluate((href, code) => {
-                const toAbsoluteUrl = (src, baseUrl) => {
-                    if (src.startsWith('http://') || src.startsWith('https://')) {
-                        return src;
-                    } else {
-                        return new URL(src, baseUrl).href;
-                    }
-                };
-                const productName = document.querySelector('#frmView > div > div.goods-header > div.top > div > h2').textContent.trim();
-                const baseUrl = window.location.origin;
-                const images = document.querySelectorAll('#detail > div.txt-manual img');
-                const productDetail = Array.from(images, img => toAbsoluteUrl(img.getAttribute('src'), baseUrl));
+            try {
+                await page.goto(href, { waitUntil: 'domcontentloaded' });
+                const productContents = await page.evaluate((item) => {
+                    const toAbsoluteUrl = (src, baseUrl) => {
+                        if (src.startsWith('http://') || src.startsWith('https://')) {
+                            return src;
+                        } else {
+                            return new URL(src, baseUrl).href;
+                        }
+                    };
+                    const baseUrl = window.location.origin;
+                    const images = document.querySelectorAll('#detail > div.txt-manual img');
+                    const newProductDetail = Array.from(images, img => toAbsoluteUrl(img.getAttribute('src'), baseUrl));
 
-                return {
-                    productName,
-                    productDetail,
-                    productHref: href,
-                    productCode: code,  // 제품 코드 추가
-                };
-            }, href, code);
-            products.push(productContents);
+                    return {
+                        id: item.id,
+                        newProductDetail,
+                        originProductDetail: item.productDetal
+                    };
+                }, item);
+                products.push(productContents);
+            } catch (error) {
+                continue;
+            }
         }
 
         console.log(JSON.stringify(products));
