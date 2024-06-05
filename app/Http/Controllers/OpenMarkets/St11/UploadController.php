f9d87382346f176eec4ebb743ab9379c948bafeb
parkchef_ <parkchansu39@gmail.com>
2024-06-05 13:01:08 +0900

    commit

--- app/Http/Controllers/OpenMarkets/St11/UploadController.php
+++ app/Http/Controllers/OpenMarkets/St11/UploadController.php
@@ -19,17 +19,52 @@ class UploadController extends Controller
         $apiKey = $account->access_key;
         $method = 'post';
         $url = 'http://api.11st.co.kr/rest/prodservices/product';
-        $data = $this->getData($products);
-        $builderResult = $ac->builder($apiKey, $method, $url, $data);
+        $outboundCode = $this->getOutboundCode($apiKey);
+        return $outboundCode;
+        foreach ($products as $product) {
+            $data = $this->getData($product);
+            $builderResult = $ac->builder($apiKey, $method, $url, $data);
+        }
         return [
             'status' => false,
             'data' => $builderResult
         ];
     }
-    private function getData($products)
+    private function getOutboundCode($apiKey)
     {
-        return [
-            'Product' => []
-        ];
+        $ac = new ApiController();
+        $method = 'get';
+        $url = 'http://api.11st.co.kr/rest/areaservice/outboundarea';
+        $builderResult = $ac->builder($apiKey, $method, $url);
+        return $builderResult;
+    }
+    private function getData($product)
+    {
+        return <<<_EOT_
+        <?xml version="1.0" encoding="euc-kr" ?>
+        <Product>
+            <selMthdCd>01</selMthdCd>
+            <dispCtgrNo>$product->code</dispCtgrNo>
+            <prdTypCd>01</prdTypCd>
+            <prdNm>$product->productName</prdNm>
+            <brand>JS</brand>
+            <rmaterialTypCd>05</rmaterialTypCd>
+            <orgnTypCd>03</orgnTypCd>
+            <suplDtyfrPrdClfCd>01</suplDtyfrPrdClfCd>
+            <prdStatCd>01</prdStatCd>
+            <minorSelCnYn>Y</minorSelCnYn>
+            <prdImage01>$product->productImage</prdImage01>
+            <htmlDetail>$product->productDetail</htmlDetail>
+            <selPrc>$product->productPrice</selPrc>
+            <dlvCnAreaCd>01</dlvCnAreaCd>
+            <dlvWyCd>01</dlvWyCd>
+            <dlvCstInstBasiCd>03</dlvCstInstBasiCd>
+            <PrdFrDlvBasiAmt>300000</PrdFrDlvBasiAmt>
+            <bndlDlvCnYn>N</bndlDlvCnYn>
+            <dlvCstPayTypCd>03</dlvCstPayTypCd>
+            <jejuDlvCst>$product->additional_shipping_fee</jejuDlvCst>
+            <islandDlvCst>$product->additional_shipping_fee</islandDlvCst>
+        </Product>
+        _EOT_;
     }
 }
