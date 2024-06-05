27263792e0df938a96ffddc98179c664cc2d3e85
sangxxjin <sj_0368@naver.com>
2024-06-05 15:57:24 +0900

    feat exchange, refund

--- app/Http/Controllers/OpenMarkets/OpenMarketShipmentController.php
+++ app/Http/Controllers/OpenMarkets/OpenMarketShipmentController.php
@@ -63,58 +63,6 @@ class OpenMarketShipmentController extends Controller
         }
         return $this->update($order->id, $deliveryCompanyId, $trackingNumber, $isRemoteArea);
     }
-    private function saveExchangeRefundTrackingInfo(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'trackingNumber' => 'required|string|min:10|max:13',
-            'deliveryCompanyId' => 'required|integer|exists:delivery_companies,id',
-            'productOrderNumber' => 'required|string|exists:orders,product_order_number'
-        ], [
-            'trackingNumber.required' => '운송장 번호는 필수 항목입니다.',
-            'trackingNumber.string' => '운송장 번호는 문자열이어야 합니다.',
-            'trackingNumber.min' => '운송장 번호는 최소 10자여야 합니다.',
-            'trackingNumber.max' => '운송장 번호는 최대 13자여야 합니다.',
-            'deliveryCompanyId' => '유효한 택배사를 선택해주세요.',
-            'productOrderNumber' => '유효한 주문이 아닙니다.',
-        ]);
-        if ($validator->fails()) {
-            return [
-                'status' => false,
-                'message' => $validator->errors()->first(),
-                'error' => $validator->errors(),
-            ];
-        }
-        $trackingNumber = $request->trackingNumber;
-        $deliveryCompanyId = $request->deliveryCompanyId;
-        $productOrderNumber = $request->productOrderNumber;
-        $isRemoteArea = $request->has('isRemoteArea') && $request->isRemoteArea === 'true';
-        $order = DB::table('orders')
-            ->where('product_order_number', $productOrderNumber)
-            ->where('delivery_status', 'PENDING')
-            ->where('type', 'PAID')
-            ->first();
-        if ($order === null) {
-            return [
-                'status' => false,
-                'message' => '취소되었거나, 이미 처리된 주문입니다.'
-            ];
-        }
-        $openMarket = DB::table('orders as o')
-            ->join('partner_orders as po', 'o.id', '=', 'po.order_id')
-            ->join('vendors as v', 'po.vendor_id', '=', 'v.id')
-            ->where('o.product_order_number', $productOrderNumber)
-            ->where('v.is_active', 'ACTIVE')
-            ->select('v.*')
-            ->first(['v.name_eng', 'v.name']);
-        if ($openMarket) {
-            $method = 'call' . ucfirst($openMarket->name_eng) . 'ShipmentApi';
-            $updateApiResult = $this->$method($request);
-            if ($updateApiResult['status'] === false) {
-                return $updateApiResult;
-            }
-        }
-        return $this->update($order->id, $deliveryCompanyId, $trackingNumber, $isRemoteArea);
-    }
     private function update($orderId, $deliveryCompanyId, $trackingNumber, $isRemoteArea)
     {
         try {
