33089ca67ec980cc3248e5f9e3b29c05bb0b455a
parkchef_ <parkchansu39@gmail.com>
2024-06-14 20:20:43 +0900

    commit

---
+++ app/Http/Controllers/DeleteQueueController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DeleteQueue;
+use Illuminate\Http\Request;
+
+class DeleteQueueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DeleteQueue $deleteQueue)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DeleteQueue $deleteQueue)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DeleteQueue $deleteQueue)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DeleteQueue $deleteQueue)
+    {
+        //
+    }
+}
