01174b1bdc34b0cf399829208b76724ce5dafcd3
parkchef_ <parkchansu39@gmail.com>
2024-06-17 05:27:41 +0900

    Update DeleteUploadedProducts.php

--- app/Console/Commands/DeleteUploadedProducts.php
+++ app/Console/Commands/DeleteUploadedProducts.php
@@ -28,11 +28,11 @@ class DeleteUploadedProducts extends Command
      */
     public function handle()
     {
-        ini_set('memory_allow', '-1');
+        ini_set('memory_limit', '-1');
         set_time_limit(0);
         // 쿠팡: 40, 스마트 스토어: 51
         $vendorId = 51;
-        $accountIds = [7, 10, 8, 9, 11, 44, 46];
+        $accountIds = [44, 46];
         $vendor = DB::table('vendors')
             ->where('id', $vendorId)
             ->first();
@@ -52,7 +52,7 @@ class DeleteUploadedProducts extends Command
     }
     protected function destroy($originProductsNo, $table)
     {
-        ini_set('memory_allow', '-1');
+        ini_set('memory_limit', '-1');
         set_time_limit(0);
         try {
             DB::table($table)
