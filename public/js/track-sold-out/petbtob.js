afee089fca720904b843b54a5e09f0b7df4a37da
parkchef_ <parkchansu39@gmail.com>
2024-06-17 06:50:04 +0900

    Update petbtob.js

--- public/js/track-sold-out/petbtob.js
+++ public/js/track-sold-out/petbtob.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     try {
         const [tempFilePath, username, password] = process.argv.slice(2);
