521c26b8c34ec5203d24a547aa607a217706cf57
sangxxjin <sj_0368@naver.com>
2024-06-24 15:06:04 +0900

    Update ApiController.php

--- app/Http/Controllers/OpenMarkets/St11/ApiController.php
+++ app/Http/Controllers/OpenMarkets/St11/ApiController.php
@@ -37,20 +37,14 @@ class ApiController extends Controller
             'openapikey' => $apiKey,
         ])->withBody(iconv('UTF-8', 'EUC-KR', $data), 'text/xml')
             ->$method($url);
-
         if ($response->successful()) {
             $euc_kr_body = $response->body();
-
             Log::info('Original Response Body (EUC-KR): ' . $euc_kr_body);
-
             // XML 인코딩 선언 변경
             $euc_kr_body = preg_replace('/<\?xml version="1.0" encoding="EUC-KR"\?>/', '<?xml version="1.0" encoding="UTF-8"?>', $euc_kr_body);
-
             // mb_convert_encoding을 사용하여 변환
             $utf8_response_body = mb_convert_encoding($euc_kr_body, 'UTF-8', 'EUC-KR');
-
             Log::info('Converted UTF-8 Response Body: ' . $utf8_response_body);
-
             if (mb_detect_encoding($utf8_response_body, 'UTF-8', true) === false) {
                 return [
                     'status' => false,
@@ -58,10 +52,8 @@ class ApiController extends Controller
                     'error' => '응답 본문을 UTF-8로 변환하는 과정에서 에러가 발생했습니다.'
                 ];
             }
-
             libxml_use_internal_errors(true);
             $xml = simplexml_load_string($utf8_response_body);
-
             if ($xml === false) {
                 $errors = libxml_get_errors();
                 $errorMessages = [];
@@ -69,7 +61,6 @@ class ApiController extends Controller
                     $errorMessages[] = $error->message;
                 }
                 libxml_clear_errors();
-
                 return [
                     'status' => false,
                     'message' => 'XML 파싱 실패',
@@ -77,18 +68,14 @@ class ApiController extends Controller
                     'response_body' => $utf8_response_body
                 ];
             }
-
             $xmlArray = json_decode(json_encode($xml), true);
-
             $jsonFormattedData = json_encode($xmlArray, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
             Log::info('Formatted Response Data: ' . $jsonFormattedData);
-
             return [
                 'status' => true,
                 'data' => $xmlArray
             ];
         }
-
         return [
             'status' => false,
             'message' => 'API 요청을 보내는 과정에서 에러가 발생했습니다.',
