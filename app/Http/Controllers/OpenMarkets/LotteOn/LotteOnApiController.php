2526f5d54baa724138e04fb6d809731cbb8e3014
sangxxjin <sj_0368@naver.com>
2024-07-02 10:20:19 +0900

    Update LotteOnApiController.php

--- app/Http/Controllers/OpenMarkets/LotteOn/LotteOnApiController.php
+++ app/Http/Controllers/OpenMarkets/LotteOn/LotteOnApiController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers\OpenMarkets\LotteOn;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 
 class LotteOnApiController extends Controller
