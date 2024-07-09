9783a8a722a500b3354355755b3f59690cc5c9eb
shoojoy <157701033+shoojoy@users.noreply.github.com>
2024-07-09 17:15:34 +0900

    Update domecall.js

--- public/js/track-sold-out/domecall.js
+++ public/js/track-sold-out/domecall.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { goToAttempts, signIn } = require('./trackwing-common');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setViewport({
         width: 1920,
