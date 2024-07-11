ecf15e714daf701607294b43828c1fca2a65b3e3
shoojoy <157701033+shoojoy@users.noreply.github.com>
2024-07-11 18:30:54 +0900

    Update pettory.js

--- public/js/track-sold-out/pettory.js
+++ public/js/track-sold-out/pettory.js
@@ -44,11 +44,27 @@ const { goToAttempts, signIn } = require('./trackwing-common');
 async function validateProduct(page) {
     try {
         return await page.evaluate(() => {
-            const txtDescElement = document.querySelector('p.txtDesc');
-            if (txtDescElement && txtDescElement.textContent.trim().includes('사라졌거나')) {
-                return false;
+            // const txtDescElement = document.querySelector('p.txtDesc');
+            // if (txtDescElement && txtDescElement.textContent.trim().includes('사라졌거나')) {
+            //     return false;
+            // }
+            // const soldOutImage = document.querySelector('div.infoArea img[src="//img.echosting.cafe24.com/design/common/icon_sellout.gif"]');
+            // if (soldOutImage) {
+            //     return false;
+            // }
+            // const buyButton = document.querySelector('a.first');
+            // if (buyButton && buyButton.classList.contains('displaynone') && buyButton.textContent.trim().includes('구매하기')) {
+            //     return false;
+            // }
+            const orderLimitElement = document.querySelector('#contents > div.xans-element-.xans-product.xans-product-detail > div.detailArea > div.infoArea > div.guideArea > p');
+            if (orderLimitElement) {
+                const orderLimitText = orderLimitElement.textContent.trim();
+                const minimumOrderMatch = orderLimitText.match(/최소주문수량\s*(\d+)\s*개 이상/);
+                if (minimumOrderMatch && parseInt(minimumOrderMatch[1], 10) > 1) {
+                    return false;
+                }
             }
-            return true;
+            return true
         });
     } catch (error) {
         return false;
